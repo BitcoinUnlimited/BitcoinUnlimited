59cf9750205ccc39451c6883d04041c54fc8ebe4
Pieter Wuille <pieter.wuille@gmail.com>
2016-03-26 13:31:25 +0100

    Only store and connect to NODE_NETWORK nodes

--- src/net.cpp
+++ src/net.cpp
@@ -1928,6 +1928,10 @@ void ThreadOpenConnections()
             if (IsLimited(addr))
                 continue;
 
+            // only connect to full nodes
+            if (!(addr.nServices & NODE_NETWORK))
+                continue;
+
             // only consider very recently tried nodes after 30 failed attempts
             if (nANow - addr.nLastTry < 600 && nTries < 30)
                 continue;
