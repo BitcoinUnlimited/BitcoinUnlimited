b1552a5fe83576cb14b41123b0110f9f860951ea
Jonas Schnelli <dev@jonasschnelli.ch>
2016-07-15 11:42:47 +0200

    [Wallet] Ensure <0.13 clients can't open HD wallets

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3417,6 +3417,9 @@ bool CWallet::InitLoadWallet()
             CPubKey masterPubKey = walletInstance->GenerateNewHDMasterKey();
             if (!walletInstance->SetHDMasterKey(masterPubKey))
                 throw std::runtime_error("CWallet::GenerateNewKey(): Storing master key failed");
+
+            // ensure this wallet.dat can only be opened by clients supporting HD
+            walletInstance->SetMinVersion(FEATURE_HD);
         }
         CPubKey newDefaultKey;
         if (walletInstance->GetKeyFromPool(newDefaultKey))
