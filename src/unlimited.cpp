4deaa92dfa56011bb69c5457c29d4831f2145d2f
Sarto <satoshi@keemail.me>
2016-08-22 00:54:17 +0300

    Update unlimited.cpp
    add 2015

--- src/unlimited.cpp
+++ src/unlimited.cpp
@@ -629,7 +629,7 @@ std::string LicenseInfo()
 {
     return FormatParagraph(strprintf(_("Copyright (C) 2009-%i The Bitcoin Core Developers"), COPYRIGHT_YEAR)) + "\n\n" +
            FormatParagraph(strprintf(_("Copyright (C) 2014-%i The Bitcoin XT Developers"), COPYRIGHT_YEAR)) + "\n\n" +
-           FormatParagraph(strprintf(_("Copyright (C) %i The Bitcoin Unlimited Developers"), COPYRIGHT_YEAR)) + "\n\n" +
+           FormatParagraph(strprintf(_("Copyright (C) 2015-%i The Bitcoin Unlimited Developers"), COPYRIGHT_YEAR)) + "\n\n" +
            "\n" +
            FormatParagraph(_("This is experimental software.")) + "\n" +
            "\n" +
