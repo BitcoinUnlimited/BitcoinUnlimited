4cfcf5fbeb0c7063db231351aab9d1fe114deb67
fanquake <fanquake@gmail.com>
2016-12-01 12:39:31 +0800

    [depends] ZeroMQ 4.2.2

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -1,13 +1,17 @@
 package=zeromq
-$(package)_version=4.0.7
-$(package)_download_path=http://download.zeromq.org
+$(package)_version=4.2.2
+$(package)_download_path=https://github.com/zeromq/libzmq/releases/download/v$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=e00b2967e074990d0538361cc79084a0a92892df2c6e7585da34e4c61ee47b03
+$(package)_sha256_hash=5b23f4ca9ef545d5bd3af55d305765e3ee06b986263b31967435d285a3e6df6b
 
 define $(package)_set_vars
-  $(package)_config_opts=--without-documentation --disable-shared
+  $(package)_config_opts=--without-docs --disable-shared --without-libsodium --disable-curve --disable-curve-keygen --disable-perf
   $(package)_config_opts_linux=--with-pic
   $(package)_cxxflags=-std=c++11
+endef
+
+define $(package)_preprocess_cmds
+  ./autogen.sh
 endef
 
 define $(package)_config_cmds
@@ -15,7 +19,7 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE) -C src
+  $(MAKE) src/libzmq.la
 endef
 
 define $(package)_stage_cmds
