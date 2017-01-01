57d1f9cb37ff98181581f612037e070bd51f4167
Justaphf <juholmes@vt.edu>
2017-01-01 17:20:41 -0500

    Add args to skip some build steps if not needed.
    1. Added "--strip" argument.  Now this script will only run the strip
    command on the generated bitcoin binaries if this argument is provided.
    2. Added “--no-autogen” argument.  If provided, this argument will skip
    execution of “./autogen.sh” to reduce build times (convenience feature
    for Windows developers).
    3. Added “--no-configure” argument.  If provided, this argument will
    skip execution of “./configure” to reduce build times (convenience
    feature for Windows developers).  If specified, this argument also
    automatically applies the “—no-autogen” argument as well.
    
    NOTE: The “--no-autogen” and “--no-configure” options should only be
    used if a successful build of the bitcoin binaries has already been
    completed in this build directory, and no configuration changes have
    been made.  Currently the script doesn't have a safety check these
    commands have run successfully, and if you have not previously run
    “./autogen.sh” and/or “./configure” successfully, the script will
    blow-up if you run with these arguments.

--- build-aux/mingw/make-bitcoin.sh
+++ build-aux/mingw/make-bitcoin.sh
@@ -22,6 +22,19 @@ case $i in
     TOOLCHAIN_BIN="${i#*=}"
     shift # past argument=value
     ;;
+    --strip)
+    STRIP=YES
+    shift # past argument=value
+    ;;
+    --no-autogen)
+    SKIP_AUTOGEN=YES
+    shift # past argument=value
+    ;;
+    --no-configure)
+	SKIP_AUTOGEN=YES # if configure is off, then we cannot run autogen
+    SKIP_CONFIGURE=YES
+    shift # past argument=value
+    ;;
     --default)
     DEFAULT=YES
     shift # past argument with no value
@@ -36,34 +49,45 @@ PATH=$TOOLCHAIN_BIN:$MSYS_BIN:$MINGW_BIN:$PATH
 
 # Build BitcoinUnlimited
 cd $PATH_BITCOIN
-./autogen.sh
-CPPFLAGS="-I$PATH_DEPS/db-4.8.30.NC/build_unix \
--I$PATH_DEPS/openssl-1.0.1k/include \
--I$PATH_DEPS/libevent-2.0.22/include \
--I$PATH_DEPS \
--I$PATH_DEPS/protobuf-2.6.1/src \
--I$PATH_DEPS/libpng-1.6.16 \
--I$PATH_DEPS/qrencode-3.4.4" \
-LDFLAGS="-L$PATH_DEPS/db-4.8.30.NC/build_unix \
--L$PATH_DEPS/openssl-1.0.1k \
--L$PATH_DEPS/libevent-2.0.22/.libs \
--L$PATH_DEPS/miniupnpc \
--L$PATH_DEPS/protobuf-2.6.1/src/.libs \
--L$PATH_DEPS/libpng-1.6.16/.libs \
--L$PATH_DEPS/qrencode-3.4.4/.libs" \
-BOOST_ROOT=$PATH_DEPS/boost_1_61_0 \
-./configure \
---disable-upnp-default \
---disable-tests \
---with-qt-incdir=$PATH_DEPS/Qt/5.3.2/include \
---with-qt-libdir=$PATH_DEPS/Qt/5.3.2/lib \
---with-qt-plugindir=$PATH_DEPS/Qt/5.3.2/plugins \
---with-qt-bindir=$PATH_DEPS/Qt/5.3.2/bin \
---with-protoc-bindir=$PATH_DEPS/protobuf-2.6.1/src
+if [ -z "$SKIP_AUTOGEN" ]; then
+	./autogen.sh
+fi
+
+BOOST_ROOT=$PATH_DEPS/boost_1_61_0
+
+if [ -z "$SKIP_CONFIGURE" ]; then
+	CPPFLAGS="-I$PATH_DEPS/db-4.8.30.NC/build_unix \
+	-I$PATH_DEPS/openssl-1.0.1k/include \
+	-I$PATH_DEPS/libevent-2.0.22/include \
+	-I$PATH_DEPS \
+	-I$PATH_DEPS/protobuf-2.6.1/src \
+	-I$PATH_DEPS/libpng-1.6.16 \
+	-I$PATH_DEPS/qrencode-3.4.4" \
+	LDFLAGS="-L$PATH_DEPS/db-4.8.30.NC/build_unix \
+	-L$PATH_DEPS/openssl-1.0.1k \
+	-L$PATH_DEPS/libevent-2.0.22/.libs \
+	-L$PATH_DEPS/miniupnpc \
+	-L$PATH_DEPS/protobuf-2.6.1/src/.libs \
+	-L$PATH_DEPS/libpng-1.6.16/.libs \
+	-L$PATH_DEPS/qrencode-3.4.4/.libs" \
+	BOOST_ROOT=$PATH_DEPS/boost_1_61_0 \
+	./configure \
+	--disable-upnp-default \
+	--disable-tests \
+	--with-qt-incdir=$PATH_DEPS/Qt/5.3.2/include \
+	--with-qt-libdir=$PATH_DEPS/Qt/5.3.2/lib \
+	--with-qt-plugindir=$PATH_DEPS/Qt/5.3.2/plugins \
+	--with-qt-bindir=$PATH_DEPS/Qt/5.3.2/bin \
+	--with-protoc-bindir=$PATH_DEPS/protobuf-2.6.1/src
+fi
 
 make -j4
 
 # Strip symbol tables
-strip src/bitcoin-cli.exe
-strip src/bitcoind.exe
-strip src/qt/bitcoin-qt.exe
+if [ -n "$STRIP" ]; then
+	echo 'Stripping exeutables'
+	strip src/bitcoin-tx.exe
+	strip src/bitcoin-cli.exe
+	strip src/bitcoind.exe
+	strip src/qt/bitcoin-qt.exe
+fi
