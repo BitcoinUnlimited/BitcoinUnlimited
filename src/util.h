ded146f67473560d9ff73c2cbaefc55abd0f233c
Wladimir J. van der Laan <laanwj@gmail.com>
2017-01-05 10:46:54 +0100

    Merge #9417: Do not evaluate hidden LogPrint arguments
    407cdd6 Do not evaluate hidden LogPrint arguments (Pieter Wuille)

--- src/util.h
+++ src/util.h
@@ -318,14 +318,15 @@ bool SetupNetworking();
 /** Return true if log accepts specified category */
 bool LogAcceptCategory(const char *category);
 
-#define LogPrintf(...) LogPrint(NULL, __VA_ARGS__)
+#define LogPrint(category, ...) do { \
+    if (LogAcceptCategory((category))) { \
+        LogPrintStr(tfm::format(__VA_ARGS__)); \
+    } \
+} while(0)
 
-template<typename... Args>
-static inline int LogPrint(const char* category, const char* fmt, const Args&... args)
-{
-    if(!LogAcceptCategory(category)) return 0;                            \
-    return LogPrintStr(tfm::format(fmt, args...));
-}
+#define LogPrintf(...) do { \
+    LogPrintStr(tfm::format(__VA_ARGS__)); \
+} while(0)
 
 template<typename... Args>
 bool error(const char* fmt, const Args&... args)
