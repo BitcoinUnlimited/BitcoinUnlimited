93273eee0c968e15cd6c62f5f6e520699e111cf1
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2017-03-22 12:56:44 -0700

    [zmq] Call va_end() on va_start()ed args.

--- src/zmq/zmqpublishnotifier.cpp
+++ src/zmq/zmqpublishnotifier.cpp
@@ -24,6 +24,7 @@ static int zmq_send_multipart(void *sock, const void *data, size_t size, ...)
         if (rc != 0)
         {
             zmqError("Unable to initialize ZMQ msg");
+            va_end(args);
             return -1;
         }
 
@@ -37,6 +38,7 @@ static int zmq_send_multipart(void *sock, const void *data, size_t size, ...)
         {
             zmqError("Unable to send ZMQ msg");
             zmq_msg_close(&msg);
+            va_end(args);
             return -1;
         }
 
@@ -47,6 +49,7 @@ static int zmq_send_multipart(void *sock, const void *data, size_t size, ...)
 
         size = va_arg(args, size_t);
     }
+    va_end(args);
     return 0;
 }
 
