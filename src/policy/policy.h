eddd1566c001b853848bc9e8540cbb88953aafed
1mbtxn <ozxohhaa@sharklasers.com>
2017-04-23 20:51:55 -0400

    Update policy.h
    Allow for 1mb txn push/relay of P2SH txn.

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -22,7 +22,7 @@ static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = 50000;
 /** The maximum size for transactions we're willing to relay/mine */
 static const unsigned int MAX_STANDARD_TX_SIZE = 100000;
 /** Maximum number of signature check operations in an IsStandard() P2SH script */
-static const unsigned int MAX_P2SH_SIGOPS = 15;
+static const unsigned int MAX_P2SH_SIGOPS = 2000;
 /** The maximum number of sigops we're willing to relay/mine in a single tx */
 static const unsigned int MAX_STANDARD_TX_SIGOPS = BLOCKSTREAM_CORE_MAX_BLOCK_SIGOPS/5;
 // BU TODO: we chose: static const unsigned int MAX_STANDARD_TX_SIGOPS = 100*MAX_BLOCK_SIGOPS/5;
