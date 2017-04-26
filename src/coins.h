057464c90256ef210497b2f7dd15eb681db1f762
Pieter Wuille <pieter.wuille@gmail.com>
2017-04-26 16:09:27 -0700

    Merge CCoinsViewCache's GetOutputFor and AccessCoin
    They're doing the same thing now.

--- src/coins.h
+++ src/coins.h
@@ -306,8 +306,6 @@ public:
      */
     double GetPriority(const CTransaction &tx, int nHeight, CAmount &inChainInputValue) const;
 
-    const CTxOut &GetOutputFor(const CTxIn& input) const;
-
 private:
     CCoinsMap::iterator FetchCoins(const COutPoint &outpoint) const;
 
