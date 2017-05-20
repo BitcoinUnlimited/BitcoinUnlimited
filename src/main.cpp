03ba92608ead37fda075d4bbd1c804a2551921f7
Neil Booth <kyuupichan@gmail.com>
2017-05-20 20:01:16 +0900

    BU-specific fixes

--- src/main.cpp
+++ src/main.cpp
@@ -5769,9 +5769,11 @@ bool ProcessMessage(CNode *pfrom, std::string strCommand, CDataStream &vRecv, in
         }
         if (pfrom->nServicesExpected & ~pfrom->nServices)
         {
-            LogPrint("net", "peer=%d does not offer the expected services (%08x offered, %08x expected); disconnecting\n", pfrom->id, pfrom->nServices, pfrom->nServicesExpected);
+            LogPrint("net",
+                "peer=%d does not offer the expected services (%08x offered, %08x expected); disconnecting\n",
+                pfrom->id, pfrom->nServices, pfrom->nServicesExpected);
             pfrom->PushMessage(NetMsgType::REJECT, strCommand, REJECT_NONSTANDARD,
-                               strprintf("Expected to offer services %08x", pfrom->nServicesExpected));
+                strprintf("Expected to offer services %08x", pfrom->nServicesExpected));
             pfrom->fDisconnect = true;
             return false;
         }
