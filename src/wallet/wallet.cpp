6b4461db0da3ba84c73c06d999dc2d0168286589
marlengit <marlen@safe-mail.net>
2017-06-08 08:19:48 +0200

    Revert ListLockedCoins const.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2946,7 +2946,7 @@ bool CWallet::IsLockedCoin(uint256 hash, unsigned int n) const
     return (setLockedCoins.count(outpt) > 0);
 }
 
-void CWallet::ListLockedCoins(std::vector<COutPoint>& vOutpts) const
+void CWallet::ListLockedCoins(std::vector<COutPoint>& vOutpts)
 {
     AssertLockHeld(cs_wallet); // setLockedCoins
     for (std::set<COutPoint>::iterator it = setLockedCoins.begin();
