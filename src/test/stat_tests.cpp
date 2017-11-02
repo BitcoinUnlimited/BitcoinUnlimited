573e0b2fed3f2e896f61e840c709ced3ca09bb67
Andrew Stone <g.andrew.stone@gmail.com>
2017-11-02 09:32:24 -0400

    revert unrelated change

--- src/test/stat_tests.cpp
+++ src/test/stat_tests.cpp
@@ -50,8 +50,10 @@ BOOST_AUTO_TEST_CASE(stat_testvectors)
     s4 += 4.3;
     BOOST_CHECK(s4() == 7.6);
 
-    // Turn off the callback.  test will call explicitly for precision
-    statMinInterval=std::chrono::milliseconds(100000);
+    // statMinInterval=boost::posix_time::milliseconds(50); // boost::posix_time::seconds(1); // Speed things up
+    // for (int i=0;i<numMetrics;i++)  doesn't work because there are other global variable stats
+    //  stat_io_service.run_one();
+    // stat_io_service.poll();
     s1.timeout(boost::system::error_code());
     s2.timeout(boost::system::error_code());
     s3.timeout(boost::system::error_code());
@@ -81,7 +83,8 @@ BOOST_AUTO_TEST_CASE(stat_testvectors)
     BOOST_CHECK(s2.History(1, 0) == (29 * 10 + 120) / 30);
     BOOST_CHECK(s3.History(1, 0) == 120000);
     BOOST_CHECK(s4.History(1, 0) == 3.3);
-    //statMinInterval = boost::posix_time::milliseconds(10); // boost::posix_time::seconds(1); // Speed things up
+
+    statMinInterval = boost::posix_time::milliseconds(10); // boost::posix_time::seconds(1); // Speed things up
     for (int i = 0; i < 12; i++)
         for (int j = 0; j < 30; j++)
         {
