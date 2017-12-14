9843c1bb5e7697abafb0975f57fbca0750642b78
Peter Tschipper <peter.tschipper@gmail.com>
2017-12-14 07:41:28 -0800

    Remove #ifdef BITCOIN_CASH for blockpriority space
    Make blockprioritysize 5% of the default blocksize
    regardless of implementation.

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -23,12 +23,8 @@ static const unsigned int DEFAULT_BLOCK_MAX_SIZE = BLOCKSTREAM_CORE_MAX_BLOCK_SI
 #endif
 static const unsigned int DEFAULT_BLOCK_MIN_SIZE = 0;
 
-/** Default for -blockprioritysize, maximum space for zero/low-fee transactions **/
-#ifdef BITCOIN_CASH
+/** Default for -blockprioritysize, a 5% maximum space for zero/low-fee transactions **/
 static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = DEFAULT_BLOCK_MAX_SIZE / 20;
-#else
-static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = 50000;
-#endif
 
 /** The maximum size for transactions we're willing to relay/mine */
 static const unsigned int MAX_STANDARD_TX_SIZE = 100000;
