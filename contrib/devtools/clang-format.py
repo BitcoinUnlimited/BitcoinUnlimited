e0377b58f6187616a89d60586c8652743e90aea6
Andrew Stone <g.andrew.stone@gmail.com>
2017-12-14 10:30:24 -0500

    use the proper name of the clang-format program instead of just clang-format.  This is needed to ensure that people run clang-format version 3.8 (#860)

--- contrib/devtools/clang-format.py
+++ contrib/devtools/clang-format.py
@@ -78,7 +78,7 @@ def run_clang_check(clang_format_exe, files):
         print("\nNonexistent files: " + ",".join(nonexistent))
     if changed:
         print("\nImproper formatting found in: " + ",".join(list(changed)))
-        print("To properly format these files run: " + sys.argv[0] + " format clang-format " + " ".join(list(changed)))
+        print("To properly format these files run: " + sys.argv[0] + " format " + clang_format_exe + " " + " ".join(list(changed)))
         return 1
     else:
         print("All existing files are properly formatted")
