e4cce85d7d8f24afb32e98082b7708b2500cc174
Andrew Stone <g.andrew.stone@gmail.com>
2017-12-15 12:32:52 -0500

    port from giga_perf: add thread synchronization primitives CSharedCriticalSection and CThreadCorral necessary to multithread transaction validation

--- src/sync.h
+++ src/sync.h
@@ -15,7 +15,8 @@
 #include <boost/thread/locks.hpp>
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/recursive_mutex.hpp>
-
+#include <boost/thread/shared_mutex.hpp>
+#include <boost/thread/thread.hpp>
 
 ////////////////////////////////////////////////
 //                                            //
@@ -69,6 +70,7 @@ public:
  */
 #ifndef DEBUG_LOCKORDER
 typedef AnnotatedMixin<boost::recursive_mutex> CCriticalSection;
+#define CRITSEC(x) CCriticalSection x
 #else // BU we need to remove the critical section from the lockorder map when destructed
 class CCriticalSection : public AnnotatedMixin<boost::recursive_mutex>
 {
@@ -78,13 +80,107 @@ public:
     CCriticalSection();
     ~CCriticalSection();
 };
+/** Define a critical section that is named in debug builds.
+    Named critical sections are useful in conjunction with a lock analyzer to discover bottlenecks. */
+#define CRITSEC(zzname) CCriticalSection zzname(#zzname)
 #endif
+
+#ifndef DEBUG_LOCKORDER
+typedef AnnotatedMixin<boost::shared_mutex> CSharedCriticalSection;
+/** Define a named, shared critical section that is named in debug builds.
+    Named critical sections are useful in conjunction with a lock analyzer to discover bottlenecks. */
+#define SCRITSEC(x) CSharedCriticalSection x
+#else
+
+/** A shared critical section allows multiple entities to take the critical section in a "shared" mode,
+    but only one entity to take the critical section exclusively.
+    This is very useful for single-writer, many reader data structures. For example most of the containers
+    in the std and boost libraries follow these access semantics.
+
+    A SharedCriticalSection is NOT recursive.
+*/
+class CSharedCriticalSection : public AnnotatedMixin<boost::shared_mutex>
+{
+public:
+    class LockInfo
+    {
+    public:
+        const char *file;
+        unsigned int line;
+        LockInfo() : file(""), line(0) {}
+        LockInfo(const char *f, unsigned int l) : file(f), line(l) {}
+    };
+
+    boost::mutex setlock;
+    std::map<uint64_t, LockInfo> sharedowners;
+    const char *name;
+    uint64_t exclusiveOwner;
+    CSharedCriticalSection(const char *name);
+    CSharedCriticalSection();
+    ~CSharedCriticalSection();
+    void lock_shared();
+    bool try_lock_shared();
+    void unlock_shared();
+    void lock();
+    void unlock();
+    bool try_lock();
+};
+#define SCRITSEC(zzname) CSharedCriticalSection zzname(#zzname)
+#endif
+
+
+// This object can be locked or shared locked some time during its lifetime.
+// Subsequent locks or shared lock calls will be ignored.
+// When it is deleted, the lock is released.
+class CDeferredSharedLocker
+{
+    enum class LockState
+    {
+        UNLOCKED,
+        SHARED,
+        EXCLUSIVE
+    };
+    CSharedCriticalSection &scs;
+    LockState state;
+
+public:
+    CDeferredSharedLocker(CSharedCriticalSection &scsp) : scs(scsp), state(LockState::UNLOCKED) {}
+    void lock_shared()
+    {
+        if (state == LockState::UNLOCKED)
+        {
+            scs.lock_shared();
+            state = LockState::SHARED;
+        }
+    }
+    void lock()
+    {
+        if (state == LockState::UNLOCKED)
+        {
+            scs.lock();
+            state = LockState::EXCLUSIVE;
+        }
+    }
+
+    void unlock()
+    {
+        if (state == LockState::SHARED)
+            scs.unlock_shared();
+        else if (state == LockState::EXCLUSIVE)
+            scs.unlock();
+        state = LockState::UNLOCKED;
+    }
+    ~CDeferredSharedLocker() { unlock(); }
+};
 
 /** Wrapped boost mutex: supports waiting but not recursive locking */
 typedef AnnotatedMixin<boost::mutex> CWaitableCriticalSection;
 
 /** Just a typedef for boost::condition_variable, can be wrapped later if desired */
 typedef boost::condition_variable CConditionVariable;
+
+/** Just a typedef for boost::condition_variable_any, can be wrapped later if desired -- c++11 version missing on win */
+typedef boost::condition_variable_any CCond;
 
 #ifdef DEBUG_LOCKORDER
 void EnterCritical(const char *pszName, const char *pszFile, int nLine, void *cs, bool fTry = false);
@@ -93,17 +189,29 @@ void LeaveCritical();
 // another CCriticalSection could be created on top of it.
 void DeleteCritical(const void *cs);
 std::string LocksHeld();
+/** Asserts in debug builds if a critical section is not held. */
 void AssertLockHeldInternal(const char *pszName, const char *pszFile, int nLine, void *cs);
+/** Asserts in debug builds if a shared critical section is not exclusively held. */
+void AssertWriteLockHeldInternal(const char *pszName, const char *pszFile, int nLine, CSharedCriticalSection *cs);
 #else
 void static inline EnterCritical(const char *pszName, const char *pszFile, int nLine, void *cs, bool fTry = false) {}
 void static inline LeaveCritical() {}
 void static inline AssertLockHeldInternal(const char *pszName, const char *pszFile, int nLine, void *cs) {}
+void static inline AssertWriteLockHeldInternal(const char *pszName,
+    const char *pszFile,
+    int nLine,
+    CSharedCriticalSection *cs)
+{
+}
 #endif
 #define AssertLockHeld(cs) AssertLockHeldInternal(#cs, __FILE__, __LINE__, &cs)
+#define AssertWriteLockHeld(cs) AssertWriteLockHeldInternal(#cs, __FILE__, __LINE__, &cs)
 
 #ifdef DEBUG_LOCKCONTENTION
 void PrintLockContention(const char *pszName, const char *pszFile, int nLine);
 #endif
+
+#define LOCK_WARN_TIME (500ULL * 1000ULL * 1000ULL)
 
 /** Wrapper around boost::unique_lock<Mutex> */
 template <typename Mutex>
@@ -111,14 +219,20 @@ class SCOPED_LOCKABLE CMutexLock
 {
 private:
     boost::unique_lock<Mutex> lock;
-    int64_t lockedTime;
+// Checking elapsed lock time is very inefficient compared to the lock/unlock operation so we must be able to
+// turn the feature on and off at compile time.
+#ifdef DEBUG_LOCKTIME
+    uint64_t lockedTime;
+#endif
     const char *name;
     const char *file;
     int line;
 
     void Enter(const char *pszName, const char *pszFile, int nLine)
     {
-        int64_t startWait = GetTimeMillis();
+#ifdef DEBUG_LOCKTIME
+        uint64_t startWait = GetStopwatch();
+#endif
         name = pszName;
         file = pszFile;
         line = nLine;
@@ -132,11 +246,14 @@ private:
 #ifdef DEBUG_LOCKCONTENTION
         }
 #endif
-        lockedTime = GetTimeMillis();
-        if (lockedTime - startWait > 500)
+
+#ifdef DEBUG_LOCKTIME
+        lockedTime = GetStopwatch();
+        if (lockedTime - startWait > LOCK_WARN_TIME)
         {
             LogPrint("lck", "Lock %s at %s:%d waited for %d ms\n", pszName, pszFile, nLine, (lockedTime - startWait));
         }
+#endif
     }
 
     bool TryEnter(const char *pszName, const char *pszFile, int nLine)
@@ -148,11 +265,15 @@ private:
         lock.try_lock();
         if (!lock.owns_lock())
         {
+#ifdef DEBUG_LOCKTIME
             lockedTime = 0;
+#endif
             LeaveCritical();
         }
+#ifdef DEBUG_LOCKTIME
         else
-            lockedTime = GetTimeMillis();
+            lockedTime = GetStopwatch();
+#endif
         return lock.owns_lock();
     }
 
@@ -185,21 +306,135 @@ public:
         if (lock.owns_lock())
         {
             LeaveCritical();
-            int64_t doneTime = GetTimeMillis();
-            if (doneTime - lockedTime > 500)
+#ifdef DEBUG_LOCKTIME
+            uint64_t doneTime = GetStopwatch();
+            if (doneTime - lockedTime > LOCK_WARN_TIME)
             {
                 LogPrint(
                     "lck", "Lock %s at %s:%d remained locked for %d ms\n", name, file, line, doneTime - lockedTime);
             }
+#endif
         }
     }
 
     operator bool() { return lock.owns_lock(); }
 };
 
+/** Wrapper around boost::unique_lock<Mutex> */
+template <typename Mutex>
+class SCOPED_LOCKABLE CMutexReadLock
+{
+private:
+    boost::shared_lock<Mutex> lock;
+    uint64_t lockedTime;
+    const char *name;
+    const char *file;
+    int line;
+
+    void Enter(const char *pszName, const char *pszFile, int nLine)
+    {
+#ifdef DEBUG_LOCKTIME
+        uint64_t startWait = GetStopwatch();
+#endif
+        name = pszName;
+        file = pszFile;
+        line = nLine;
+        EnterCritical(pszName, pszFile, nLine, (void *)(lock.mutex()));
+// LogPrint("lck","try ReadLock %p %s by %d\n", lock.mutex(), name ? name : "", boost::this_thread::get_id());
+#ifdef DEBUG_LOCKCONTENTION
+        if (!lock.try_lock())
+        {
+            PrintLockContention(pszName, pszFile, nLine);
+#endif
+            lock.lock();
+#ifdef DEBUG_LOCKCONTENTION
+        }
+#endif
+// LogPrint("lck","ReadLock %p %s taken by %d\n", lock.mutex(), name ? name : "", boost::this_thread::get_id());
+#ifdef DEBUG_LOCKTIME
+        lockedTime = GetStopwatch();
+        if (lockedTime - startWait > LOCK_WARN_TIME)
+        {
+            LogPrint("lck", "Lock %s at %s:%d waited for %d ms\n", pszName, pszFile, nLine, (lockedTime - startWait));
+        }
+#endif
+    }
+
+    bool TryEnter(const char *pszName, const char *pszFile, int nLine)
+    {
+        name = pszName;
+        file = pszFile;
+        line = nLine;
+        EnterCritical(pszName, pszFile, nLine, (void *)(lock.mutex()), true);
+        if (!lock.try_lock())
+        {
+#ifdef DEBUG_LOCKTIME
+            lockedTime = 0;
+#endif
+            LeaveCritical();
+        }
+#ifdef DEBUG_LOCKTIME
+        else
+            lockedTime = GetStopwatch();
+#endif
+        return lock.owns_lock();
+    }
+
+public:
+    CMutexReadLock(Mutex &mutexIn, const char *pszName, const char *pszFile, int nLine, bool fTry = false)
+        SHARED_LOCK_FUNCTION(mutexIn)
+        : lock(mutexIn, boost::defer_lock)
+    {
+        if (fTry)
+            TryEnter(pszName, pszFile, nLine);
+        else
+            Enter(pszName, pszFile, nLine);
+    }
+
+    CMutexReadLock(Mutex *pmutexIn, const char *pszName, const char *pszFile, int nLine, bool fTry = false)
+        SHARED_LOCK_FUNCTION(pmutexIn)
+    {
+        if (!pmutexIn)
+            return;
+
+        lock = boost::shared_lock<Mutex>(*pmutexIn, boost::defer_lock);
+        if (fTry)
+            TryEnter(pszName, pszFile, nLine);
+        else
+            Enter(pszName, pszFile, nLine);
+    }
+
+    ~CMutexReadLock() UNLOCK_FUNCTION()
+    {
+        if (lock.owns_lock())
+        {
+            LeaveCritical();
+#ifdef DEBUG_LOCKTIME
+            int64_t doneTime = GetStopwatch();
+            if (doneTime - lockedTime > LOCK_WARN_TIME)
+            {
+                LogPrint(
+                    "lck", "Lock %s at %s:%d remained locked for %d ms\n", name, file, line, doneTime - lockedTime);
+            }
+#endif
+        }
+        // When lock is destructed it will release
+    }
+
+    operator bool() { return lock.owns_lock(); }
+};
+
+typedef CMutexReadLock<CSharedCriticalSection> CReadBlock;
+typedef CMutexLock<CSharedCriticalSection> CWriteBlock;
 typedef CMutexLock<CCriticalSection> CCriticalBlock;
 
-#define LOCK(cs) CCriticalBlock criticalblock(cs, #cs, __FILE__, __LINE__)
+#define READLOCK(cs) CReadBlock UNIQUIFY(readblock)(cs, #cs, __FILE__, __LINE__)
+#define WRITELOCK(cs) CWriteBlock UNIQUIFY(writeblock)(cs, #cs, __FILE__, __LINE__)
+#define READLOCK2(cs1, cs2) \
+    CReadBlock readblock1(cs1, #cs1, __FILE__, __LINE__), readblock2(cs2, #cs2, __FILE__, __LINE__)
+#define TRY_READ_LOCK(cs, name) CReadBlock name(cs, #cs, __FILE__, __LINE__, true)
+
+#define LOCK(cs) CCriticalBlock UNIQUIFY(criticalblock)(cs, #cs, __FILE__, __LINE__)
 #define LOCK2(cs1, cs2) \
     CCriticalBlock criticalblock1(cs1, #cs1, __FILE__, __LINE__), criticalblock2(cs2, #cs2, __FILE__, __LINE__)
 #define TRY_LOCK(cs, name) CCriticalBlock name(cs, #cs, __FILE__, __LINE__, true)
@@ -324,4 +559,83 @@ private:
 
 typedef std::vector<std::pair<void *, CLockLocation> > LockStack;
 typedef std::map<std::pair<void *, void *>, LockStack> LockStackMap;
+
+/** A thread corral is a granular thread organization technique.
+Code is assigned to a corral via Enter(...) and Exit(...) APIs (but use the scoped CCorralLock object instead of direct
+calls).
+Multiple threads can be running in the same corral, but threads cannot run in multiple corrals simultaneously.
+
+Higher corral numbers block lower ones, but are themselves blocked from entry until the all other corrals are clear.
+For example, lets us assume threads are running in corral region 1.
+If a thread wants to enter corral region 2, threads are blocked from entering region 1.  Once all threads have left 1,
+the thread(s) waiting to enter 2 are released.
+Now, a thread wants to enter corral region 1.  Threads can continue to enter and leave region 2 (since it is > 1).
+If all threads leave region 2, the threads waiting for region 1 are released.
+
+Higher corral numbers are used to implement higher priority tasks.
+*/
+class CThreadCorral
+{
+protected:
+    int curRegion;
+    int curCount;
+    int maxRequestedRegion;
+    CCriticalSection mutex;
+    CCond cond;
+
+public:
+    CThreadCorral() : curRegion(0), curCount(0), maxRequestedRegion(0) {}
+    void Enter(int region)
+    {
+        LOCK(mutex);
+        while (1)
+        {
+            // If no region is running and I'm the biggest requested region, then run my region
+            if ((curCount == 0) && (region >= maxRequestedRegion))
+            {
+                curRegion = region;
+                maxRequestedRegion = 0;
+                curCount = 1;
+                return;
+            }
+            // If the current region is mine, and no higher priority regions want to run, then I can run
+            else if ((curRegion == region) && (region >= maxRequestedRegion))
+            {
+                curCount++;
+                return;
+            }
+            else // I can't run now.
+            {
+                if (region > maxRequestedRegion)
+                    maxRequestedRegion = region;
+                cond.wait(mutex);
+            }
+        }
+    }
+
+    void Exit(int region)
+    {
+        LOCK(mutex);
+        assert(curRegion == region);
+        curCount--;
+        if (curCount == 0)
+        {
+            cond.notify_all();
+        }
+    }
+};
+
+class CCorralLock
+{
+protected:
+    CThreadCorral &corral;
+    int region;
+
+public:
+    CCorralLock(CThreadCorral &corralp, int regionp) : corral(corralp), region(regionp) { corral.Enter(region); }
+    ~CCorralLock() { corral.Exit(region); }
+};
+
+#define CORRAL(cral, region) CCorralLock UNIQUIFY(corral)(cral, region);
+
 #endif // BITCOIN_SYNC_H
