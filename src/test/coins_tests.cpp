05652890bfdc98b87560b5be3572def5aebdf404
Peter Tschipper <peter.tschipper@gmail.com>
2017-12-15 05:33:32 -0800

    Tidy up a few comments in coins_tests.cpp

--- src/test/coins_tests.cpp
+++ src/test/coins_tests.cpp
@@ -227,9 +227,9 @@ BOOST_AUTO_TEST_CASE(coins_cache_simulation_test)
             }
         }
 
+        // Every 100 iterations, flush an intermediate cache
         if (insecure_rand() % 100 == 0)
         {
-            // Every 100 iterations, flush an intermediate cache
             if (stack.size() > 1 && insecure_rand() % 2 == 0)
             {
                 unsigned int flushIndex = insecure_rand() % (stack.size() - 1);
