Bitcoin-ABC/bitcoin-abc

Make Config more available in Qt
Test Plan: ran qt client

Reviewers: #bitcoin_abc, deadalnix, schancel

Reviewed By: #bitcoin_abc, deadalnix, schancel

Subscribers: deadalnix

Differential Revision: https://reviews.bitcoinabc.org/D708

src/qt/bitcoingui.cpp
@@ -80,7 +80,7 @@ const std::string BitcoinGUI::DEFAULT_UIPLATFORM =
  */
 const QString BitcoinGUI::DEFAULT_WALLET = "~Default";
 
-BitcoinGUI::BitcoinGUI(const PlatformStyle *_platformStyle,
+BitcoinGUI::BitcoinGUI(const Config *cfg, const PlatformStyle *_platformStyle,
                        const NetworkStyle *networkStyle, QWidget *parent)
     : QMainWindow(parent), enableWallet(false), clientModel(0), walletFrame(0),
       unitDisplayControl(0), labelWalletEncryptionIcon(0),
@@ -95,7 +95,7 @@ BitcoinGUI::BitcoinGUI(const PlatformStyle *_platformStyle,
       aboutQtAction(0), openRPCConsoleAction(0), openAction(0),
       showHelpMessageAction(0), trayIcon(0), trayIconMenu(0), notificator(0),
       rpcConsole(0), helpMessageDialog(0), modalOverlay(0), prevBlocks(0),
-      spinnerFrame(0), platformStyle(_platformStyle) {
+      spinnerFrame(0), platformStyle(_platformStyle), cfg(cfg) {
     GUIUtil::restoreWindowGeometry("nWindow", QSize(850, 550), this);
 
     QString windowTitle = tr(PACKAGE_NAME) + " - ";
@@ -127,7 +127,7 @@ BitcoinGUI::BitcoinGUI(const PlatformStyle *_platformStyle,
 #ifdef ENABLE_WALLET
     if (enableWallet) {
         /** Create wallet frame and make it the central widget */
-        walletFrame = new WalletFrame(_platformStyle, this);
+        walletFrame = new WalletFrame(_platformStyle, cfg, this);
         setCentralWidget(walletFrame);
     } else
 #endif // ENABLE_WALLET
@@ -712,7 +712,7 @@ void BitcoinGUI::showHelpMessageClicked() {
 
 #ifdef ENABLE_WALLET
 void BitcoinGUI::openClicked() {
-    OpenURIDialog dlg(this);
+    OpenURIDialog dlg(cfg, this);
     if (dlg.exec()) {
         Q_EMIT receivedURI(dlg.getURI());
     }

BitcoinUnlimited/BitcoinUnlimited

src/qt/bitcoingui.cpp
@@ -67,7 +67,7 @@
 
 const QString BitcoinGUI::DEFAULT_WALLET = "~Default";
 
-BitcoinGUI::BitcoinGUI(const PlatformStyle *platformStyle, const NetworkStyle *networkStyle, QWidget *parent)
+BitcoinGUI::BitcoinGUI(const Config *cfg, const PlatformStyle *platformStyle, const NetworkStyle *networkStyle, QWidget *parent)
     : QMainWindow(parent), clientModel(0), walletFrame(0), unitDisplayControl(0), labelEncryptionIcon(0),
       labelConnectionsIcon(0), labelBlocksIcon(0), progressBarLabel(0), progressBar(0), progressDialog(0),
       appMenuBar(0), overviewAction(0), historyAction(0), quitAction(0), sendCoinsAction(0), sendCoinsMenuAction(0),
@@ -115,7 +115,7 @@ BitcoinGUI::BitcoinGUI(const PlatformStyle *platformStyle, const NetworkStyle *n
     if (enableWallet)
     {
         /** Create wallet frame and make it the central widget */
-        walletFrame = new WalletFrame(platformStyle, this);
+        walletFrame = new WalletFrame(platformStyle, cfg, this);
         setCentralWidget(walletFrame);
     }
     else
@@ -619,7 +619,7 @@ void BitcoinGUI::showHelpMessageClicked() { helpMessageDialog->show(); }
 #ifdef ENABLE_WALLET
 void BitcoinGUI::openClicked()
 {
-    OpenURIDialog dlg(this);
+    OpenURIDialog dlg(cfg, this);
     if (dlg.exec())
     {
         Q_EMIT receivedURI(dlg.getURI());