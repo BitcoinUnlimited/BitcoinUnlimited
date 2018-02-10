455a76f192d7fb577090bf10163acd3bf89bcff9
fanquake <fanquake@gmail.com>
2018-02-10 09:00:14 +0800

    [depends] ccache 3.4.1

--- depends/packages/native_ccache.mk
+++ depends/packages/native_ccache.mk
@@ -1,8 +1,8 @@
 package=native_ccache
-$(package)_version=3.2.4
+$(package)_version=3.4.1
 $(package)_download_path=https://samba.org/ftp/ccache
 $(package)_file_name=ccache-$($(package)_version).tar.bz2
-$(package)_sha256_hash=ffeb967edb549e67da0bd5f44f729a2022de9fdde65dfd80d2a7204d7f75332e
+$(package)_sha256_hash=ca5a01fb4868cdb5176c77b8b4a390be7929a6064be80741217e0686f03f8389
 
 define $(package)_set_vars
 $(package)_config_opts=
