4171184b3c2183ded177f012c28e2ace31dfde40
Amaury Séchet <deadalnix@gmail.com>
2018-03-17 01:41:05 +0100

    Add a flag to gate opcodes activation for the new HF
    Summary: This per se does nothing except adding a flag to gate opcodes and the infrastructure to use this flag in tests.
    
    Test Plan: Test involving the flag have been added for all disabled opcodes.
    
    Reviewers: #bitcoin_abc, schancel
    
    Reviewed By: #bitcoin_abc, schancel
    
    Subscribers: teamcity
    
    Differential Revision: https://reviews.bitcoinabc.org/D1208

---
+++ src/test/scriptflags.cpp
@@ -0,0 +1,69 @@
+// Copyright (c) 2017 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "test/scriptflags.h"
+
+#include "script/interpreter.h"
+
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <map>
+#include <vector>
+
+static std::map<std::string, uint32_t> mapFlagNames = {
+    {"NONE", SCRIPT_VERIFY_NONE}, {"P2SH", SCRIPT_VERIFY_P2SH}, {"STRICTENC", SCRIPT_VERIFY_STRICTENC},
+    {"DERSIG", SCRIPT_VERIFY_DERSIG}, {"LOW_S", SCRIPT_VERIFY_LOW_S}, {"SIGPUSHONLY", SCRIPT_VERIFY_SIGPUSHONLY},
+    {"MINIMALDATA", SCRIPT_VERIFY_MINIMALDATA}, {"NULLDUMMY", SCRIPT_VERIFY_NULLDUMMY},
+    {"DISCOURAGE_UPGRADABLE_NOPS", SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_NOPS}, {"CLEANSTACK", SCRIPT_VERIFY_CLEANSTACK},
+    {"MINIMALIF", SCRIPT_VERIFY_MINIMALIF}, {"NULLFAIL", SCRIPT_VERIFY_NULLFAIL},
+    {"CHECKLOCKTIMEVERIFY", SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY},
+    {"CHECKSEQUENCEVERIFY", SCRIPT_VERIFY_CHECKSEQUENCEVERIFY},
+    {"DISCOURAGE_UPGRADABLE_WITNESS_PROGRAM", SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_WITNESS_PROGRAM},
+    {"COMPRESSED_PUBKEYTYPE", SCRIPT_VERIFY_COMPRESSED_PUBKEYTYPE}, {"SIGHASH_FORKID", SCRIPT_ENABLE_SIGHASH_FORKID},
+    {"REPLAY_PROTECTION", SCRIPT_ENABLE_REPLAY_PROTECTION}, {"MONOLITH_OPCODES", SCRIPT_ENABLE_MONOLITH_OPCODES},
+};
+
+uint32_t ParseScriptFlags(std::string strFlags)
+{
+    if (strFlags.empty())
+    {
+        return 0;
+    }
+
+    uint32_t flags = 0;
+    std::vector<std::string> words;
+    boost::algorithm::split(words, strFlags, boost::algorithm::is_any_of(","));
+
+    for (std::string &word : words)
+    {
+        if (!mapFlagNames.count(word))
+            BOOST_ERROR("Bad test: unknown verification flag '" << word << "'");
+        flags |= mapFlagNames[word];
+    }
+
+    return flags;
+}
+
+std::string FormatScriptFlags(uint32_t flags)
+{
+    if (flags == 0)
+    {
+        return "";
+    }
+
+    std::string ret;
+    std::map<std::string, uint32_t>::const_iterator it = mapFlagNames.begin();
+    while (it != mapFlagNames.end())
+    {
+        if (flags & it->second)
+        {
+            ret += it->first + ",";
+        }
+        it++;
+    }
+
+    return ret.substr(0, ret.size() - 1);
+}
