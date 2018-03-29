a5d9cdcb4e298ec3b813ad1a9e008ac179776ea5
joshuayabut <yabut.joshua@gmail.com>
2018-03-29 22:26:06 +0200

    Add support for CAT
    Summary:
    Add handling for CAT opcode when the monolith script flag is enabled.
    
    Co-authored-by: Joshua Yabut <yabut.joshua@gmail.com>
    Co-authored-by: Marcos Mayorga <mm@mm-studios.com>
    
    Test Plan: `make check`
    
    Reviewers: #bitcoin_abc, O1 Bitcoin ABC, movrcx, jasonbcox
    
    Reviewed By: #bitcoin_abc, O1 Bitcoin ABC, jasonbcox
    
    Subscribers: jasonbcox, shadders, danconnolly, teamcity, macros
    
    Maniphest Tasks: T307
    
    Differential Revision: https://reviews.bitcoinabc.org/D1227

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -295,7 +295,6 @@ static inline bool IsOpcodeDisabled(opcodetype opcode, uint32_t flags)
 {
     switch (opcode)
     {
-    case OP_CAT:
     case OP_SPLIT:
     case OP_INVERT:
     case OP_2MUL:
@@ -308,6 +307,7 @@ static inline bool IsOpcodeDisabled(opcodetype opcode, uint32_t flags)
         // Disabled opcodes
         return true;
 
+    case OP_CAT:
     case OP_AND:
     case OP_OR:
     case OP_XOR:
@@ -1211,6 +1211,28 @@ bool EvalScript(vector<vector<unsigned char> > &stack,
                     }
                 }
                 break;
+
+                //
+                // Byte string operations
+                //
+                case OP_CAT:
+                {
+                    // (x1 x2 -- out)
+                    if (stack.size() < 2)
+                    {
+                        return set_error(serror, SCRIPT_ERR_INVALID_STACK_OPERATION);
+                    }
+                    valtype &vch1 = stacktop(-2);
+                    valtype &vch2 = stacktop(-1);
+                    if (vch1.size() + vch2.size() > MAX_SCRIPT_ELEMENT_SIZE)
+                    {
+                        return set_error(serror, SCRIPT_ERR_PUSH_SIZE);
+                    }
+                    vch1.insert(vch1.end(), vch2.begin(), vch2.end());
+                    popstack(stack);
+                }
+                break;
+
 
                 //
                 // Conversion operations
