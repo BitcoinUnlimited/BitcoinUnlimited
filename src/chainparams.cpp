d23bf1c70bff7976fcad2ce8052be7d91df84d9b
sanch0panza <sanch0panza@protonmail.com>
2018-05-17 09:28:11 +0200

    Remove BOM from chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin Core developers
 // Copyright (c) 2015-2018 The Bitcoin Unlimited developers
 // Distributed under the MIT software license, see the accompanying
