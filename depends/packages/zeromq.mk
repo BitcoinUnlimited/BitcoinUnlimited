349acaa8f2d40052954b2e966f04a73db0795b65
Andrew Stone <g.andrew.stone@gmail.com>
2018-06-12 12:45:16 -0400

    Revert "[PORT] Depends cherries (#1120)"
    This reverts commit 964f326bb1dd3b33a562079ad22ad1e46503bc7f.

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -1,19 +1,13 @@
 package=zeromq
-$(package)_version=4.2.3
-$(package)_download_path=https://github.com/zeromq/libzmq/releases/download/v$($(package)_version)/
+$(package)_version=4.0.7
+$(package)_download_path=http://download.zeromq.org
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=8f1e2b2aade4dbfde98d82366d61baef2f62e812530160d2e6d0a5bb24e40bc0
-$(package)_patches=0001-fix-build-with-older-mingw64.patch 0002-disable-pthread_set_name_np.patch
+$(package)_sha256_hash=e00b2967e074990d0538361cc79084a0a92892df2c6e7585da34e4c61ee47b03
 
 define $(package)_set_vars
-  $(package)_config_opts=--without-docs --disable-shared --without-libsodium --disable-curve --disable-curve-keygen --disable-perf
+  $(package)_config_opts=--without-documentation --disable-shared
   $(package)_config_opts_linux=--with-pic
   $(package)_cxxflags=-std=c++11
-endef
-
-define $(package)_preprocess_cmds
-   patch -p1 < $($(package)_patch_dir)/0001-fix-build-with-older-mingw64.patch && \
-   patch -p1 < $($(package)_patch_dir)/0002-disable-pthread_set_name_np.patch
 endef
 
 define $(package)_config_cmds
@@ -21,14 +15,13 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE) src/libzmq.la
+  $(MAKE) -C src
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install-libLTLIBRARIES install-includeHEADERS install-pkgconfigDATA
+  $(MAKE) -C src DESTDIR=$($(package)_staging_dir) install
 endef
 
 define $(package)_postprocess_cmds
-  sed -i.old "s/ -lstdc++//" lib/pkgconfig/libzmq.pc && \
   rm -rf bin share
 endef
--- doc/build-osx.md
+++ doc/build-osx.md
@@ -18,9 +18,7 @@ Dependencies
 
     brew install automake berkeley-db4 libtool boost --c++11 miniupnpc openssl pkg-config protobuf --c++11 qt5 libevent
 
-See [dependencies.md](dependencies.md) for a complete overview.
-
-If you want to build the disk image with `make deploy` (.dmg / optional), you need RSVG
+In case you want to build the disk image with `make deploy` (.dmg / optional), you need RSVG
 
     brew install librsvg
 
