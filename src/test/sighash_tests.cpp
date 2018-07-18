ca0c174aa40a43e283005cdb17126664c5b7412c
Awemany <awemany@protonmail.com>
2018-07-18 10:28:51 +0200

    testutil: Export RandomScript(..) and RandomTransaction(..)
    Make these two available to other tests as well.

--- src/test/sighash_tests.cpp
+++ src/test/sighash_tests.cpp
@@ -14,6 +14,7 @@
 #include "streams.h"
 #include "test/test_bitcoin.h"
 #include "test/test_random.h"
+#include "test/testutil.h"
 #include "util.h"
 #include "utilstrencodings.h"
 #include "version.h"
@@ -87,42 +88,6 @@ uint256 static SignatureHashOld(CScript scriptCode, const CTransaction &txTo, un
     CHashWriter ss(SER_GETHASH, 0);
     ss << txTmp << nHashType;
     return ss.GetHash();
-}
-
-void static RandomScript(CScript &script)
-{
-    static const opcodetype oplist[] = {
-        OP_FALSE, OP_1, OP_2, OP_3, OP_CHECKSIG, OP_IF, OP_VERIF, OP_RETURN, OP_CODESEPARATOR};
-    script = CScript();
-    int ops = (insecure_rand() % 10);
-    for (int i = 0; i < ops; i++)
-        script << oplist[insecure_rand() % (sizeof(oplist) / sizeof(oplist[0]))];
-}
-
-void static RandomTransaction(CMutableTransaction &tx, bool fSingle)
-{
-    tx.nVersion = insecure_rand();
-    tx.vin.clear();
-    tx.vout.clear();
-    tx.nLockTime = (insecure_rand() % 2) ? insecure_rand() : 0;
-    int ins = (insecure_rand() % 4) + 1;
-    int outs = fSingle ? ins : (insecure_rand() % 4) + 1;
-    for (int in = 0; in < ins; in++)
-    {
-        tx.vin.push_back(CTxIn());
-        CTxIn &txin = tx.vin.back();
-        txin.prevout.hash = GetRandHash();
-        txin.prevout.n = insecure_rand() % 4;
-        RandomScript(txin.scriptSig);
-        txin.nSequence = (insecure_rand() % 2) ? insecure_rand() : (unsigned int)-1;
-    }
-    for (int out = 0; out < outs; out++)
-    {
-        tx.vout.push_back(CTxOut());
-        CTxOut &txout = tx.vout.back();
-        txout.nValue = insecure_rand() % 100000000;
-        RandomScript(txout.scriptPubKey);
-    }
 }
 
 BOOST_FIXTURE_TEST_SUITE(sighash_tests, BasicTestingSetup)
--- src/test/testutil.cpp
+++ src/test/testutil.cpp
@@ -3,6 +3,8 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "testutil.h"
+#include "primitives/transaction.h"
+#include "test/test_random.h"
 
 #ifdef WIN32
 #include <shlobj.h>
@@ -11,3 +13,38 @@
 #include "fs.h"
 
 fs::path GetTempPath() { return fs::temp_directory_path(); }
+void RandomScript(CScript &script)
+{
+    static const opcodetype oplist[] = {
+        OP_FALSE, OP_1, OP_2, OP_3, OP_CHECKSIG, OP_IF, OP_VERIF, OP_RETURN, OP_CODESEPARATOR};
+    script = CScript();
+    int ops = (insecure_rand() % 10);
+    for (int i = 0; i < ops; i++)
+        script << oplist[insecure_rand() % (sizeof(oplist) / sizeof(oplist[0]))];
+}
+
+void RandomTransaction(CMutableTransaction &tx, bool fSingle)
+{
+    tx.nVersion = insecure_rand();
+    tx.vin.clear();
+    tx.vout.clear();
+    tx.nLockTime = (insecure_rand() % 2) ? insecure_rand() : 0;
+    int ins = (insecure_rand() % 4) + 1;
+    int outs = fSingle ? ins : (insecure_rand() % 4) + 1;
+    for (int in = 0; in < ins; in++)
+    {
+        tx.vin.push_back(CTxIn());
+        CTxIn &txin = tx.vin.back();
+        txin.prevout.hash = GetRandHash();
+        txin.prevout.n = insecure_rand() % 4;
+        RandomScript(txin.scriptSig);
+        txin.nSequence = (insecure_rand() % 2) ? insecure_rand() : (unsigned int)-1;
+    }
+    for (int out = 0; out < outs; out++)
+    {
+        tx.vout.push_back(CTxOut());
+        CTxOut &txout = tx.vout.back();
+        txout.nValue = insecure_rand() % 100000000;
+        RandomScript(txout.scriptPubKey);
+    }
+}
