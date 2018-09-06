BitcoinUnlimited/BitcoinUnlimited

commit 85288e202fa5de5ac94e81505cc1ef94b454b95c

Tidy up basic xthinblock checking and use atomics for tracking requests (#1296)
* Add basic message checking to getdata:msg_thinblock messages
* Switch to using atomics and remove cs_vNodes lock
Increment by 1 at the same time you're calculating the
decay value. No need to increment after.
* clean up comments

src/main.cpp
@@ -4892,6 +4892,39 @@ void static ProcessGetData(CNode *pfrom, const Consensus::Params &consensusParam
     }
 }
 
+static bool BasicThinblockChecks(CNode *pfrom, const CChainParams &chainparams)
+{
+    if (!pfrom->ThinBlockCapable())
+    {
+        dosMan.Misbehaving(pfrom, 100);
+        return error("Thinblock message received from a non thinblock node, peer=%d", pfrom->GetId());
+    }
+
+    // Check for Misbehaving and DOS
+    // If they make more than 20 requests in 10 minutes then disconnect them
+    {
+        if (pfrom->nGetXthinLastTime <= 0)
+            pfrom->nGetXthinLastTime = GetTime();
+        uint64_t nNow = GetTime();
+        double tmp = pfrom->nGetXthinCount;
+        while (pfrom->nGetXthinCount.compare_exchange_weak(
+            tmp, (tmp * std::pow(1.0 - 1.0 / 600.0, (double)(nNow - pfrom->nGetXthinLastTime)) + 1)))
+            ;
+        pfrom->nGetXthinLastTime = nNow;
+        LOG(THIN, "nGetXthinCount is %f\n", pfrom->nGetXthinCount);
+        if (chainparams.NetworkIDString() == "main") // other networks have variable mining rates
+        {
+            if (pfrom->nGetXthinCount >= 20)
+            {
+                dosMan.Misbehaving(pfrom, 50); // If they exceed the limit then disconnect them
+                return error("requesting too many getdata thinblocks");
+            }
+        }
+    }
+
+    return true;
+}
+
 bool ProcessMessage(CNode *pfrom, std::string strCommand, CDataStream &vRecv, int64_t nTimeReceived)
 {
     int64_t receiptTime = GetTime();
@@ -5338,18 +5371,24 @@ bool ProcessMessage(CNode *pfrom, std::string strCommand, CDataStream &vRecv, in
                 dosMan.Misbehaving(pfrom, 20);
                 return error("message inv invalid type = %u", inv.type);
             }
-            // inv.hash does not need validation, since SHA2556 hash can be any value
+
+            // Make basic checks
+            if (inv.type == MSG_THINBLOCK)
+            {
+                if (!BasicThinblockChecks(pfrom, chainparams))
+                    return false;
+            }
+
+
+            if (fDebug || (vInv.size() != 1))
+                LOG(NET, "received getdata (%u invsz) peer=%d\n", vInv.size(), pfrom->id);
+
+            if ((fDebug && vInv.size() > 0) || (vInv.size() == 1))
+                LOG(NET, "received getdata for: %s peer=%d\n", vInv[0].ToString(), pfrom->id);
+
+            pfrom->vRecvGetData.insert(pfrom->vRecvGetData.end(), vInv.begin(), vInv.end());
+            ProcessGetData(pfrom, chainparams.GetConsensus());
         }
-
-
-        if (fDebug || (vInv.size() != 1))
-            LOG(NET, "received getdata (%u invsz) peer=%d\n", vInv.size(), pfrom->id);
-
-        if ((fDebug && vInv.size() > 0) || (vInv.size() == 1))
-            LOG(NET, "received getdata for: %s peer=%d\n", vInv[0].ToString(), pfrom->id);
-
-        pfrom->vRecvGetData.insert(pfrom->vRecvGetData.end(), vInv.begin(), vInv.end());
-        ProcessGetData(pfrom, chainparams.GetConsensus());
     }
 
 
@@ -5915,32 +5954,8 @@ bool ProcessMessage(CNode *pfrom, std::string strCommand, CDataStream &vRecv, in
     // BUIP010 Xtreme Thinblocks: begin section
     else if (strCommand == NetMsgType::GET_XTHIN && !fImporting && !fReindex && IsThinBlocksEnabled())
     {
-        if (!pfrom->ThinBlockCapable())
-        {
-            dosMan.Misbehaving(pfrom, 100);
-            return error("Thinblock message received from a non thinblock node, peer=%d", pfrom->GetId());
-        }
-
-        // Check for Misbehaving and DOS
-        // If they make more than 20 requests in 10 minutes then disconnect them
-        {
-            LOCK(cs_vNodes);
-            if (pfrom->nGetXthinLastTime <= 0)
-                pfrom->nGetXthinLastTime = GetTime();
-            uint64_t nNow = GetTime();
-            pfrom->nGetXthinCount *= std::pow(1.0 - 1.0 / 600.0, (double)(nNow - pfrom->nGetXthinLastTime));
-            pfrom->nGetXthinLastTime = nNow;
-            pfrom->nGetXthinCount += 1;
-            LOG(THIN, "nGetXthinCount is %f\n", pfrom->nGetXthinCount);
-            if (chainparams.NetworkIDString() == "main") // other networks have variable mining rates
-            {
-                if (pfrom->nGetXthinCount >= 20)
-                {
-                    dosMan.Misbehaving(pfrom, 100); // If they exceed the limit then disconnect them
-                    return error("requesting too many get_xthin");
-                }
-            }
-        }
+        if (!BasicThinblockChecks(pfrom, chainparams))
+            return false;
 
         CBloomFilter filterMemPool;
         CInv inv;
@@ -5990,8 +6005,6 @@ bool ProcessMessage(CNode *pfrom, std::string strCommand, CDataStream &vRecv, in
     {
         return HandleExpeditedRequest(vRecv, pfrom);
     }
-
-
     else if (strCommand == NetMsgType::XPEDITEDBLK)
     {
         // ignore the expedited message unless we are at the chain tip...

src/blockrelay/graphene.cpp
@@ -234,14 +234,14 @@ bool CRequestGrapheneBlockTx::HandleMessage(CDataStream &vRecv, CNode *pfrom)
     // Check for Misbehaving and DOS
     // If they make more than 20 requests in 10 minutes then disconnect them
     {
-        LOCK(cs_vNodes);
         if (pfrom->nGetGrapheneBlockTxLastTime <= 0)
             pfrom->nGetGrapheneBlockTxLastTime = GetTime();
         uint64_t nNow = GetTime();
-        pfrom->nGetGrapheneBlockTxCount *=
-            std::pow(1.0 - 1.0 / 600.0, (double)(nNow - pfrom->nGetGrapheneBlockTxLastTime));
+        double tmp = pfrom->nGetGrapheneBlockTxCount;
+        while (pfrom->nGetGrapheneBlockTxCount.compare_exchange_weak(
+            tmp, (tmp * std::pow(1.0 - 1.0 / 600.0, (double)(nNow - pfrom->nGetGrapheneBlockTxLastTime)) + 1)))
+            ;
         pfrom->nGetGrapheneBlockTxLastTime = nNow;
-        pfrom->nGetGrapheneBlockTxCount += 1;
         LOG(GRAPHENE, "nGetGrapheneTxCount is %f\n", pfrom->nGetGrapheneBlockTxCount);
         if (pfrom->nGetGrapheneBlockTxCount >= 20)
         {
@@ -1449,13 +1449,14 @@ bool HandleGrapheneBlockRequest(CDataStream &vRecv, CNode *pfrom, const CChainPa
     // Check for Misbehaving and DOS
     // If they make more than 20 requests in 10 minutes then disconnect them
     {
-        LOCK(cs_vNodes);
         if (pfrom->nGetGrapheneLastTime <= 0)
             pfrom->nGetGrapheneLastTime = GetTime();
         uint64_t nNow = GetTime();
-        pfrom->nGetGrapheneCount *= std::pow(1.0 - 1.0 / 600.0, (double)(nNow - pfrom->nGetGrapheneLastTime));
+        double tmp = pfrom->nGetGrapheneCount;
+        while (pfrom->nGetGrapheneCount.compare_exchange_weak(
+            tmp, (tmp * std::pow(1.0 - 1.0 / 600.0, (double)(nNow - pfrom->nGetGrapheneLastTime)) + 1)))
+            ;
         pfrom->nGetGrapheneLastTime = nNow;
-        pfrom->nGetGrapheneCount += 1;
         LOG(GRAPHENE, "nGetGrapheneCount is %f\n", pfrom->nGetGrapheneCount);
         if (chainparams.NetworkIDString() == "main") // other networks have variable mining rates
         {

src/blockrelay/thinblock.cpp
@@ -463,13 +463,14 @@ bool CXRequestThinBlockTx::HandleMessage(CDataStream &vRecv, CNode *pfrom)
     // Check for Misbehaving and DOS
     // If they make more than 20 requests in 10 minutes then disconnect them
     {
-        LOCK(cs_vNodes);
         if (pfrom->nGetXBlockTxLastTime <= 0)
             pfrom->nGetXBlockTxLastTime = GetTime();
         uint64_t nNow = GetTime();
-        pfrom->nGetXBlockTxCount *= std::pow(1.0 - 1.0 / 600.0, (double)(nNow - pfrom->nGetXBlockTxLastTime));
+        double tmp = pfrom->nGetXBlockTxCount;
+        while (pfrom->nGetXBlockTxCount.compare_exchange_weak(
+            tmp, (tmp * std::pow(1.0 - 1.0 / 600.0, (double)(nNow - pfrom->nGetXBlockTxLastTime)) + 1)))
+            ;
         pfrom->nGetXBlockTxLastTime = nNow;
-        pfrom->nGetXBlockTxCount += 1;
         LOG(THIN, "nGetXBlockTxCount is %f\n", pfrom->nGetXBlockTxCount);
         if (pfrom->nGetXBlockTxCount >= 20)
         {

src/net.h
@@ -400,10 +400,10 @@ public:
     CCriticalSection cs_mapthinblocksinflight;
     std::map<uint256, CThinBlockInFlight> mapThinBlocksInFlight GUARDED_BY(cs_mapthinblocksinflight);
 
-    double nGetXBlockTxCount; // Count how many get_xblocktx requests are made
-    uint64_t nGetXBlockTxLastTime; // The last time a get_xblocktx request was made
-    double nGetXthinCount; // Count how many get_xthin requests are made
-    uint64_t nGetXthinLastTime; // The last time a get_xthin request was made
+    std::atomic<double> nGetXBlockTxCount; // Count how many get_xblocktx requests are made
+    std::atomic<uint64_t> nGetXBlockTxLastTime; // The last time a get_xblocktx request was made
+    std::atomic<double> nGetXthinCount; // Count how many get_xthin requests are made
+    std::atomic<uint64_t> nGetXthinLastTime; // The last time a get_xthin request was made
     uint32_t nXthinBloomfilterSize; // The maximum xthin bloom filter size (in bytes) that our peer will accept.
     // BUIP010 Xtreme Thinblocks: end section
 
@@ -422,10 +422,10 @@ public:
     CCriticalSection cs_mapgrapheneblocksinflight;
     std::map<uint256, CGrapheneBlockInFlight> mapGrapheneBlocksInFlight GUARDED_BY(cs_mapgrapheneblocksinflight);
 
-    double nGetGrapheneBlockTxCount; // Count how many get_xblocktx requests are made
-    uint64_t nGetGrapheneBlockTxLastTime; // The last time a get_xblocktx request was made
-    double nGetGrapheneCount; // Count how many get_graphene requests are made
-    uint64_t nGetGrapheneLastTime; // The last time a get_graphene request was made
+    std::atomic<double> nGetGrapheneBlockTxCount; // Count how many get_xblocktx requests are made
+    std::atomic<uint64_t> nGetGrapheneBlockTxLastTime; // The last time a get_xblocktx request was made
+    std::atomic<double> nGetGrapheneCount; // Count how many get_graphene requests are made
+    std::atomic<uint64_t> nGetGrapheneLastTime; // The last time a get_graphene request was made
     uint32_t nGrapheneBloomfilterSize; // The maximum graphene bloom filter size (in bytes) that our peer will accept.
     // BUIPXXX Graphene blocks: end section
 

commit 3b3f2841d882b18a5613b67a6c4bca2846b070c4

fix bug introduced in #1296

src/main.cpp
@@ -5378,17 +5378,16 @@ bool ProcessMessage(CNode *pfrom, std::string strCommand, CDataStream &vRecv, in
                 if (!BasicThinblockChecks(pfrom, chainparams))
                     return false;
             }
-
-
-            if (fDebug || (vInv.size() != 1))
-                LOG(NET, "received getdata (%u invsz) peer=%d\n", vInv.size(), pfrom->id);
-
-            if ((fDebug && vInv.size() > 0) || (vInv.size() == 1))
-                LOG(NET, "received getdata for: %s peer=%d\n", vInv[0].ToString(), pfrom->id);
-
-            pfrom->vRecvGetData.insert(pfrom->vRecvGetData.end(), vInv.begin(), vInv.end());
-            ProcessGetData(pfrom, chainparams.GetConsensus());
         }
+
+        if (fDebug || (vInv.size() != 1))
+            LOG(NET, "received getdata (%u invsz) peer=%d\n", vInv.size(), pfrom->id);
+
+        if ((fDebug && vInv.size() > 0) || (vInv.size() == 1))
+            LOG(NET, "received getdata for: %s peer=%d\n", vInv[0].ToString(), pfrom->id);
+
+        pfrom->vRecvGetData.insert(pfrom->vRecvGetData.end(), vInv.begin(), vInv.end());
+        ProcessGetData(pfrom, chainparams.GetConsensus());
     }
 
 