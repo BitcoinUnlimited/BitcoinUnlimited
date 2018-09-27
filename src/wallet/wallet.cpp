fd4902d5f85f3dcd7fd07d01a9665e916cf10e2b
marlengit <marlen@safe-mail.net>
2018-09-27 03:53:18 +0000

    Remove tx notify support for isTopPublicLabel.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -892,19 +892,22 @@ bool CWallet::AddToWallet(const CWalletTx &wtxIn, bool fFromLoadWallet, CWalletD
             }
             else return false;
         }
-        // Break debit/credit balance caches:
-        wtx.MarkDirty();
-
-        // Notify UI of new or updated transaction
-        NotifyTransactionChanged(this, hash, fInsertedNew ? CT_NEW : CT_UPDATED);
-
-        // notify an external script when a wallet transaction comes in or is updated
-        std::string strCmd = GetArg("-walletnotify", "");
-
-        if (!strCmd.empty())
+        if (!isTopPublicLabel)
         {
-            boost::replace_all(strCmd, "%s", wtxIn.GetHash().GetHex());
-            boost::thread t(runCommand, strCmd); // thread runs free
+            // Break debit/credit balance caches:
+            wtx.MarkDirty();
+
+            // Notify UI of new or updated transaction
+            NotifyTransactionChanged(this, hash, fInsertedNew ? CT_NEW : CT_UPDATED);
+
+            // notify an external script when a wallet transaction comes in or is updated
+            std::string strCmd = GetArg("-walletnotify", "");
+
+            if (!strCmd.empty())
+            {
+                boost::replace_all(strCmd, "%s", wtxIn.GetHash().GetHex());
+                boost::thread t(runCommand, strCmd); // thread runs free
+            }
         }
     }
     return true;
