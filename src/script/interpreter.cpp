2fdd0c5e308974a6c7a4e098285266041bffa98a
Amaury Séchet <deadalnix@gmail.com>
2018-10-06 12:05:07 +0200

    Import Bitcoin ABC's reworked signature encoding check function
    Import this as IsValidSignatureEncodingWithoutSigHash(..). To be
    used for CDS/-V.
    
    This is the function that mainly resulted from this rework commit
    in Bitcoin ABC:
    
    49c7219314243398b9630ded78e97ebb0e3da776
    
    Committer's notice: The author has been set to keep the attribution
    of the author doing the rework. Also the signature has been changed
    to work on vector<uint8_t>s again.
    
    Co-authored-by: Awemany <awemany@protonmail.com>

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -202,6 +202,164 @@ bool static IsValidSignatureEncoding(const std::vector<unsigned char> &sig)
     return true;
 }
 
+
+//! Check signature encoding without sighash byte
+//! This is a copy of Bitcoin ABC's code, written mainly by deadalnix, from
+//! revision: f8283a3f284fc4722c1d6583b8746a17831d3bd0
+/**
+ * A canonical signature exists of: <30> <total len> <02> <len R> <R> <02> <len
+ * S> <S> <hashtype>, where R and S are not negative (their first byte has its
+ * highest bit not set), and not excessively padded (do not start with a 0 byte,
+ * unless an otherwise negative number follows, in which case a single 0 byte is
+ * necessary and even required).
+ *
+ * See https://bitcointalk.org/index.php?topic=8392.msg127623#msg127623
+ *
+ * This function is consensus-critical since BIP66.
+ */
+bool IsValidSignatureEncodingWithoutSigHash(const valtype &sig)
+{
+    // Format: 0x30 [total-length] 0x02 [R-length] [R] 0x02 [S-length] [S]
+    // * total-length: 1-byte length descriptor of everything that follows,
+    // excluding the sighash byte.
+    // * R-length: 1-byte length descriptor of the R value that follows.
+    // * R: arbitrary-length big-endian encoded R value. It must use the
+    // shortest possible encoding for a positive integers (which means no null
+    // bytes at the start, except a single one when the next byte has its
+    // highest bit set).
+    // * S-length: 1-byte length descriptor of the S value that follows.
+    // * S: arbitrary-length big-endian encoded S value. The same rules apply.
+
+    // Minimum and maximum size constraints.
+    if (sig.size() < 8 || sig.size() > 72)
+    {
+        return false;
+    }
+
+    //
+    // Check that the signature is a compound structure of proper size.
+    //
+
+    // A signature is of type 0x30 (compound).
+    if (sig[0] != 0x30)
+    {
+        return false;
+    }
+
+    // Make sure the length covers the entire signature.
+    // Remove:
+    // * 1 byte for the coupound type.
+    // * 1 byte for the length of the signature.
+    if (sig[1] != sig.size() - 2)
+    {
+        return false;
+    }
+
+    //
+    // Check that R is an positive integer of sensible size.
+    //
+
+    // Check whether the R element is an integer.
+    if (sig[2] != 0x02)
+    {
+        return false;
+    }
+
+    // Extract the length of the R element.
+    const uint32_t lenR = sig[3];
+
+    // Zero-length integers are not allowed for R.
+    if (lenR == 0)
+    {
+        return false;
+    }
+
+    // Negative numbers are not allowed for R.
+    if (sig[4] & 0x80)
+    {
+        return false;
+    }
+
+    // Make sure the length of the R element is consistent with the signature
+    // size.
+    // Remove:
+    // * 1 byte for the coumpound type.
+    // * 1 byte for the length of the signature.
+    // * 2 bytes for the integer type of R and S.
+    // * 2 bytes for the size of R and S.
+    // * 1 byte for S itself.
+    if (lenR > (sig.size() - 7))
+    {
+        return false;
+    }
+
+    // Null bytes at the start of R are not allowed, unless R would otherwise be
+    // interpreted as a negative number.
+    //
+    // /!\ This check can only be performed after we checked that lenR is
+    //     consistent with the size of the signature or we risk to access out of
+    //     bound elements.
+    if (lenR > 1 && (sig[4] == 0x00) && !(sig[5] & 0x80))
+    {
+        return false;
+    }
+
+    //
+    // Check that S is an positive integer of sensible size.
+    //
+
+    // S's definition starts after R's definition:
+    // * 1 byte for the coumpound type.
+    // * 1 byte for the length of the signature.
+    // * 1 byte for the size of R.
+    // * lenR bytes for R itself.
+    // * 1 byte to get to S.
+    const uint32_t startS = lenR + 4;
+
+    // Check whether the S element is an integer.
+    if (sig[startS] != 0x02)
+    {
+        return false;
+    }
+
+    // Extract the length of the S element.
+    const uint32_t lenS = sig[startS + 1];
+
+    // Zero-length integers are not allowed for S.
+    if (lenS == 0)
+    {
+        return false;
+    }
+
+    // Negative numbers are not allowed for S.
+    if (sig[startS + 2] & 0x80)
+    {
+        return false;
+    }
+
+    // Verify that the length of S is consistent with the size of the signature
+    // including metadatas:
+    // * 1 byte for the integer type of S.
+    // * 1 byte for the size of S.
+    if (size_t(startS + lenS + 2) != sig.size())
+    {
+        return false;
+    }
+
+    // Null bytes at the start of S are not allowed, unless S would otherwise be
+    // interpreted as a negative number.
+    //
+    // /!\ This check can only be performed after we checked that lenR and lenS
+    //     are consistent with the size of the signature or we risk to access
+    //     out of bound elements.
+    if (lenS > 1 && (sig[startS + 2] == 0x00) && !(sig[startS + 3] & 0x80))
+    {
+        return false;
+    }
+
+    return true;
+}
+
 bool static IsLowDERSignature(const valtype &vchSig, ScriptError *serror)
 {
     if (!IsValidSignatureEncoding(vchSig))
