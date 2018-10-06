d8dd77c07c4c264e649462a2bc5e26203dc43ed5
Peter Tschipper <peter.tschipper@gmail.com>
2018-10-06 14:15:44 -0700

    Clarify forks.cpp. Use pindexTip rather than pindexPrev.
    pindexPrev was a carry over from the original fork where we used the
    pindexPrev from the block to determine whether it was fork time. However
    we don't just use the forking logic for blocks, it's also for different
    types of transactions that may need to be validated. So, to be clear
    we can just call this what it is, the current chaintip, or pindexTip.

--- src/validation/forks.cpp
+++ src/validation/forks.cpp
@@ -5,37 +5,36 @@
 #include "forks.h"
 #include "unlimited.h"
 
+// For pindexTip use the current chainActive.Tip().
+
 bool IsDAAEnabled(const Consensus::Params &consensusparams, int nHeight)
 {
     return nHeight >= consensusparams.daaHeight;
 }
 
-bool IsDAAEnabled(const Consensus::Params &consensusparams, const CBlockIndex *pindexPrev)
+bool IsDAAEnabled(const Consensus::Params &consensusparams, const CBlockIndex *pindexTip)
 {
-    if (pindexPrev == nullptr)
+    if (pindexTip == nullptr)
     {
         return false;
     }
-
-    return IsDAAEnabled(consensusparams, pindexPrev->nHeight);
+    return IsDAAEnabled(consensusparams, pindexTip->nHeight);
 }
 
-bool IsNov152018Enabled(const Consensus::Params &consensusparams, const CBlockIndex *pindexPrev)
+bool IsNov152018Enabled(const Consensus::Params &consensusparams, const CBlockIndex *pindexTip)
 {
-    if (pindexPrev == nullptr)
+    if (pindexTip == nullptr)
     {
         return false;
     }
-
-    return pindexPrev->IsforkActiveOnNextBlock(miningForkTime.Value());
+    return pindexTip->IsforkActiveOnNextBlock(miningForkTime.Value());
 }
 
-bool IsNov152018Next(const Consensus::Params &consensusparams, const CBlockIndex *pindexPrev)
+bool IsNov152018Next(const Consensus::Params &consensusparams, const CBlockIndex *pindexTip)
 {
-    if (pindexPrev == nullptr)
+    if (pindexTip == nullptr)
     {
         return false;
     }
-
-    return pindexPrev->forkAtNextBlock(miningForkTime.Value());
+    return pindexTip->forkAtNextBlock(miningForkTime.Value());
 }
