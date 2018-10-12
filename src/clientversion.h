e703c88744a5eb7c3c0fa4caf83c7ac81d86322d
Andrea Suisani <sickpig@gmail.com>
2018-10-12 16:38:41 +0200

    Bump version to 1.5.0.0

--- src/clientversion.h
+++ src/clientversion.h
@@ -16,7 +16,7 @@
 
 //! These need to be macros, as clientversion.cpp's and bitcoin*-res.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR 1
-#define CLIENT_VERSION_MINOR 4
+#define CLIENT_VERSION_MINOR 5
 #define CLIENT_VERSION_REVISION 0
 #define CLIENT_VERSION_BUILD 0 // BU set version 99 to indicate an unreleased version
 
@@ -27,7 +27,7 @@
  * Copyright year (2009-this)
  * Todo: update this when changing our copyright comments in the source
  */
-#define COPYRIGHT_YEAR 2017
+#define COPYRIGHT_YEAR 2018
 
 #endif // HAVE_CONFIG_H
 
