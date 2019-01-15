6818d3f63ffe97a044a05dce768a48dc3b3cebfe
awemany <awemany@protonmail.com>
2019-01-15 19:34:23 +0100

    net_processing: Remove gotWorkDone variable (#1520)
    Found to be not used by clang static analysis.

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -66,10 +66,8 @@ bool PeerHasHeader(const CNodeState *state, CBlockIndex *pindex)
     return false;
 }
 
-bool static ProcessGetData(CNode *pfrom, const Consensus::Params &consensusParams, std::deque<CInv> &vInv)
+void static ProcessGetData(CNode *pfrom, const Consensus::Params &consensusParams, std::deque<CInv> &vInv)
 {
-    bool gotWorkDone = false;
-
     std::vector<CInv> vNotFound;
 
     while (!vInv.empty())
@@ -85,7 +83,6 @@ bool static ProcessGetData(CNode *pfrom, const Consensus::Params &consensusParam
         vInv.pop_front();
         {
             boost::this_thread::interruption_point();
-            gotWorkDone = true;
             if (inv.type == MSG_BLOCK || inv.type == MSG_FILTERED_BLOCK || inv.type == MSG_THINBLOCK)
             {
                 bool fSend = false;
@@ -276,7 +273,6 @@ bool static ProcessGetData(CNode *pfrom, const Consensus::Params &consensusParam
         // having to download the entire memory pool.
         pfrom->PushMessage(NetMsgType::NOTFOUND, vNotFound);
     }
-    return gotWorkDone;
 }
 
 
@@ -1847,13 +1843,12 @@ bool ProcessMessages(CNode *pfrom)
     //  (x) data
     //
     bool fOk = true;
-    bool gotWorkDone = false;
 
     {
         TRY_LOCK(pfrom->csRecvGetData, locked);
         if (locked && !pfrom->vRecvGetData.empty())
         {
-            gotWorkDone |= ProcessGetData(pfrom, chainparams.GetConsensus(), pfrom->vRecvGetData);
+            ProcessGetData(pfrom, chainparams.GetConsensus(), pfrom->vRecvGetData);
         }
     }
 
@@ -1882,7 +1877,6 @@ bool ProcessMessages(CNode *pfrom)
             pfrom->vRecvMsg.pop_front();
             pfrom->currentRecvMsgSize -= msg.size();
             msgsProcessed++;
-            gotWorkDone = true;
         }
 
         // if (fDebug)
