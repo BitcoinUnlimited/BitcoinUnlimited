65aa16c449da018526555ca4138ed100258be168
Taylor Hornby <taylor@defuse.ca>
2019-01-23 12:20:49 -0700

    Update Rust from v1.28.0 to v1.32.0.

--- depends/packages/rust.mk
+++ depends/packages/rust.mk
@@ -1,8 +1,8 @@
 package=rust
-$(package)_version=1.16.0
+$(package)_version=1.32.0
 $(package)_download_path=https://static.rust-lang.org/dist
 $(package)_file_name=rust-$($(package)_version)-x86_64-unknown-linux-gnu.tar.gz
-$(package)_sha256_hash=48621912c242753ba37cad5145df375eeba41c81079df46f93ffb4896542e8fd
+$(package)_sha256_hash=e024698320d76b74daf0e6e71be3681a1e7923122e3ebd03673fcac3ecc23810
 
 define $(package)_stage_cmds
   ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native --disable-ldconfig
