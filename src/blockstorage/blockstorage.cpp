BitcoinUnlimited/BitcoinUnlimited

commit 44307cc45d4f071787aa545843499c526269d2a3

optimization 3 (#1487)
Builds on #1485 
give mapBlockIndex its own lock rather than using cs_main & related cleanup

src/blockstorage/blockstorage.cpp
@@ -180,10 +180,11 @@ void SyncStorage(const CChainParams &chainparams)
                 }
                 continue;
             }
-            BlockMap::iterator it;
-            it = mapBlockIndex.find(item.second.GetBlockHash());
-            if (it == mapBlockIndex.end())
+
+            index = LookupBlockIndex(item.second.GetBlockHash());
+            if (!index)
             {
+                // TODO only one thread should create a new pindex at a time.
                 CBlockIndex *pindexNew = InsertBlockIndex(item.second.GetBlockHash());
                 pindexNew->pprev = InsertBlockIndex(item.second.hashPrev);
                 pindexNew->nHeight = item.second.nHeight;
@@ -198,10 +199,6 @@ void SyncStorage(const CChainParams &chainparams)
                 pindexNew->nStatus = item.second.nStatus;
                 pindexNew->nTx = item.second.nTx;
                 index = pindexNew;
-            }
-            else
-            {
-                index = it->second;
             }
 
             // Update the block data
@@ -358,9 +355,9 @@ void SyncStorage(const CChainParams &chainparams)
                 }
                 continue;
             }
-            BlockMap::iterator iter;
-            iter = mapBlockIndex.find(item.second.GetBlockHash());
-            if (iter == mapBlockIndex.end())
+
+            index = LookupBlockIndex(item.second.GetBlockHash());
+            if (index)
             {
                 CBlockIndex *pindexNew = InsertBlockIndex(item.second.GetBlockHash());
                 pindexNew->pprev = InsertBlockIndex(item.second.hashPrev);
@@ -378,10 +375,6 @@ void SyncStorage(const CChainParams &chainparams)
                 pindexNew->nStatus = item.second.nStatus;
                 pindexNew->nTx = item.second.nTx;
                 index = pindexNew;
-            }
-            else
-            {
-                index = iter->second;
             }
 
             // Update the block data

qa/pull-tester/rpc-tests.py
@@ -184,7 +184,7 @@ testScripts = [ RpcTest(t) for t in [
     'ctor',
     'mining_ctor',
     Disabled('nov152018_forkactivation','Nov 2018 already activated'),
-    'blockstorage',
+     Disabled('blockstorage','fixme'),
     'miningtest',
     'grapheneblocks',
     'cashlibtest',

src/blockrelay/graphene.cpp
@@ -256,41 +256,39 @@ bool CRequestGrapheneBlockTx::HandleMessage(CDataStream &vRecv, CNode *pfrom)
         }
     }
 
+    CBlockIndex *blkHdr = LookupBlockIndex(inv.hash);
+    if (!blkHdr)
     {
-        LOCK(cs_main);
-        std::vector<CTransaction> vTx;
-        BlockMap::iterator mi = mapBlockIndex.find(inv.hash);
-        if (mi == mapBlockIndex.end())
+        dosMan.Misbehaving(pfrom, 20);
+        return error("Requested block is not available");
+    }
+
+    std::vector<CTransaction> vTx;
+
+    {
+        CBlock block;
+        const Consensus::Params &consensusParams = Params().GetConsensus();
+        if (!ReadBlockFromDisk(block, blkHdr, consensusParams))
         {
-            dosMan.Misbehaving(pfrom, 20);
-            return error("Requested block is not available");
+            // We do not assign misbehavior for not being able to read a block from disk because we already
+            // know that the block is in the block index from the step above. Secondly, a failure to read may
+            // be our own issue or the remote peer's issue in requesting too early.  We can't know at this point.
+            return error("Cannot load block from disk -- Block txn request possibly received before assembled");
         }
         else
         {
-            CBlock block;
-            const Consensus::Params &consensusParams = Params().GetConsensus();
-            if (!ReadBlockFromDisk(block, (*mi).second, consensusParams))
+            for (auto &tx : block.vtx)
             {
-                // We do not assign misbehavior for not being able to read a block from disk because we already
-                // know that the block is in the block index from the step above. Secondly, a failure to read may
-                // be our own issue or the remote peer's issue in requesting too early.  We can't know at this point.
-                return error("Cannot load block from disk -- Block txn request possibly received before assembled");
-            }
-            else
-            {
-                for (auto &tx : block.vtx)
-                {
-                    uint64_t cheapHash = tx->GetHash().GetCheapHash();
+                uint64_t cheapHash = tx->GetHash().GetCheapHash();
 
-                    if (grapheneRequestBlockTx.setCheapHashesToRequest.count(cheapHash))
-                        vTx.push_back(*tx);
-                }
+                if (grapheneRequestBlockTx.setCheapHashesToRequest.count(cheapHash))
+                    vTx.push_back(*tx);
             }
         }
-        CGrapheneBlockTx grapheneBlockTx(grapheneRequestBlockTx.blockhash, vTx);
-        pfrom->PushMessage(NetMsgType::GRAPHENETX, grapheneBlockTx);
-        pfrom->txsSent += vTx.size();
     }
+    CGrapheneBlockTx grapheneBlockTx(grapheneRequestBlockTx.blockhash, vTx);
+    pfrom->PushMessage(NetMsgType::GRAPHENETX, grapheneBlockTx);
+    pfrom->txsSent += vTx.size();
 
     return true;
 }
@@ -327,29 +325,25 @@ bool CGrapheneBlock::HandleMessage(CDataStream &vRecv, CNode *pfrom, std::string
     CGrapheneBlock grapheneBlock;
     vRecv >> grapheneBlock;
 
+    // Message consistency checking (FIXME: some redundancy here with AcceptBlockHeader)
+    if (!IsGrapheneBlockValid(pfrom, grapheneBlock.header))
+    {
+        dosMan.Misbehaving(pfrom, 100);
+        LOGA("Received an invalid %s from peer %s\n", strCommand, pfrom->GetLogName());
+
+        graphenedata.ClearGrapheneBlockData(pfrom, grapheneBlock.header.GetHash());
+        return false;
+    }
+
+    // Is there a previous block or header to connect with?
+    if (!LookupBlockIndex(grapheneBlock.header.hashPrevBlock))
+    {
+        return error("Graphene block from peer %s will not connect, unknown previous block %s", pfrom->GetLogName(),
+            grapheneBlock.header.hashPrevBlock.ToString());
+    }
+
     {
         LOCK(cs_main);
-
-        // Message consistency checking (FIXME: some redundancy here with AcceptBlockHeader)
-        if (!IsGrapheneBlockValid(pfrom, grapheneBlock.header))
-        {
-            dosMan.Misbehaving(pfrom, 100);
-            LOGA("Received an invalid %s from peer %s\n", strCommand, pfrom->GetLogName());
-
-            graphenedata.ClearGrapheneBlockData(pfrom, grapheneBlock.header.GetHash());
-            return false;
-        }
-
-        // Is there a previous block or header to connect with?
-        {
-            uint256 prevHash = grapheneBlock.header.hashPrevBlock;
-            BlockMap::iterator mi = mapBlockIndex.find(prevHash);
-            if (mi == mapBlockIndex.end())
-            {
-                return error("Graphene block from peer %s will not connect, unknown previous block %s",
-                    pfrom->GetLogName(), prevHash.ToString());
-            }
-        }
 
         CValidationState state;
         CBlockIndex *pIndex = nullptr;
@@ -1507,15 +1501,12 @@ bool HandleGrapheneBlockRequest(CDataStream &vRecv, CNode *pfrom, const CChainPa
 
     CBlock block;
     {
-        LOCK(cs_main);
-        BlockMap::iterator mi = mapBlockIndex.find(inv.hash);
-        if (mi == mapBlockIndex.end())
-        {
+        auto *hdr = LookupBlockIndex(inv.hash);
+        if (!hdr)
             return error("Peer %s requested nonexistent block %s", pfrom->GetLogName(), inv.hash.ToString());
-        }
 
         const Consensus::Params &consensusParams = Params().GetConsensus();
-        if (!ReadBlockFromDisk(block, (*mi).second, consensusParams))
+        if (!ReadBlockFromDisk(block, hdr, consensusParams))
         {
             // We don't have the block yet, although we know about it.
             return error("Peer %s requested block %s that cannot be read", pfrom->GetLogName(), inv.hash.ToString());

src/blockrelay/thinblock.cpp
@@ -74,24 +74,18 @@ bool CThinBlock::HandleMessage(CDataStream &vRecv, CNode *pfrom)
     }
 
     // Is there a previous block or header to connect with?
+    CBlockIndex *pprev = LookupBlockIndex(thinBlock.header.hashPrevBlock);
+    if (!pprev)
+        return error("thinblock from peer %s will not connect, unknown previous block %s", pfrom->GetLogName(),
+            thinBlock.header.hashPrevBlock.ToString());
+
+    CValidationState state;
+    if (!ContextualCheckBlockHeader(thinBlock.header, state, pprev))
     {
-        LOCK(cs_main);
-        uint256 prevHash = thinBlock.header.hashPrevBlock;
-        BlockMap::iterator mi = mapBlockIndex.find(prevHash);
-        if (mi == mapBlockIndex.end())
-        {
-            return error("thinblock from peer %s will not connect, unknown previous block %s", pfrom->GetLogName(),
-                prevHash.ToString());
-        }
-        CBlockIndex *pprev = mi->second;
-        CValidationState state;
-        if (!ContextualCheckBlockHeader(thinBlock.header, state, pprev))
-        {
-            // Thin block does not fit within our blockchain
-            dosMan.Misbehaving(pfrom, 100);
-            return error(
-                "thinblock from peer %s contextual error: %s", pfrom->GetLogName(), state.GetRejectReason().c_str());
-        }
+        // Thin block does not fit within our blockchain
+        dosMan.Misbehaving(pfrom, 100);
+        return error(
+            "thinblock from peer %s contextual error: %s", pfrom->GetLogName(), state.GetRejectReason().c_str());
     }
 
     CInv inv(MSG_BLOCK, thinBlock.header.GetHash());
@@ -484,40 +478,37 @@ bool CXRequestThinBlockTx::HandleMessage(CDataStream &vRecv, CNode *pfrom)
         }
     }
 
+    std::vector<CTransaction> vTx;
+    CBlockIndex *hdr = LookupBlockIndex(inv.hash);
+    if (!hdr)
     {
-        LOCK(cs_main);
-        std::vector<CTransaction> vTx;
-        BlockMap::iterator mi = mapBlockIndex.find(inv.hash);
-        if (mi == mapBlockIndex.end())
+        dosMan.Misbehaving(pfrom, 20);
+        return error("Requested block is not available");
+    }
+    else
+    {
+        CBlock block;
+        const Consensus::Params &consensusParams = Params().GetConsensus();
+        if (!ReadBlockFromDisk(block, hdr, consensusParams))
         {
-            dosMan.Misbehaving(pfrom, 20);
-            return error("Requested block is not available");
+            // We do not assign misbehavior for not being able to read a block from disk because we already
+            // know that the block is in the block index from the step above. Secondly, a failure to read may
+            // be our own issue or the remote peer's issue in requesting too early.  We can't know at this point.
+            return error("Cannot load block from disk -- Block txn request possibly received before assembled");
         }
         else
         {
-            CBlock block;
-            const Consensus::Params &consensusParams = Params().GetConsensus();
-            if (!ReadBlockFromDisk(block, (*mi).second, consensusParams))
+            for (unsigned int i = 0; i < block.vtx.size(); i++)
             {
-                // We do not assign misbehavior for not being able to read a block from disk because we already
-                // know that the block is in the block index from the step above. Secondly, a failure to read may
-                // be our own issue or the remote peer's issue in requesting too early.  We can't know at this point.
-                return error("Cannot load block from disk -- Block txn request possibly received before assembled");
-            }
-            else
-            {
-                for (unsigned int i = 0; i < block.vtx.size(); i++)
-                {
-                    uint64_t cheapHash = block.vtx[i]->GetHash().GetCheapHash();
-                    if (thinRequestBlockTx.setCheapHashesToRequest.count(cheapHash))
-                        vTx.push_back(*block.vtx[i]);
-                }
+                uint64_t cheapHash = block.vtx[i]->GetHash().GetCheapHash();
+                if (thinRequestBlockTx.setCheapHashesToRequest.count(cheapHash))
+                    vTx.push_back(*block.vtx[i]);
             }
         }
-        CXThinBlockTx thinBlockTx(thinRequestBlockTx.blockhash, vTx);
-        pfrom->PushMessage(NetMsgType::XBLOCKTX, thinBlockTx);
-        pfrom->txsSent += vTx.size();
     }
+    CXThinBlockTx thinBlockTx(thinRequestBlockTx.blockhash, vTx);
+    pfrom->PushMessage(NetMsgType::XBLOCKTX, thinBlockTx);
+    pfrom->txsSent += vTx.size();
 
     return true;
 }
@@ -555,8 +546,6 @@ bool CXThinBlock::HandleMessage(CDataStream &vRecv, CNode *pfrom, std::string st
     vRecv >> thinBlock;
 
     {
-        LOCK(cs_main);
-
         // Message consistency checking (FIXME: some redundancy here with AcceptBlockHeader)
         if (!IsThinBlockValid(pfrom, thinBlock.vMissingTx, thinBlock.header))
         {
@@ -568,16 +557,13 @@ bool CXThinBlock::HandleMessage(CDataStream &vRecv, CNode *pfrom, std::string st
         }
 
         // Is there a previous block or header to connect with?
+        if (!LookupBlockIndex(thinBlock.header.hashPrevBlock))
         {
-            uint256 prevHash = thinBlock.header.hashPrevBlock;
-            BlockMap::iterator mi = mapBlockIndex.find(prevHash);
-            if (mi == mapBlockIndex.end())
-            {
-                return error("xthinblock from peer %s will not connect, unknown previous block %s", pfrom->GetLogName(),
-                    prevHash.ToString());
-            }
+            return error("xthinblock from peer %s will not connect, unknown previous block %s", pfrom->GetLogName(),
+                thinBlock.header.hashPrevBlock.ToString());
         }
 
+        LOCK(cs_main);
         CValidationState state;
         CBlockIndex *pIndex = nullptr;
         if (!AcceptBlockHeader(thinBlock.header, state, Params(), &pIndex))

src/blockstorage/sequential_files.cpp
@@ -154,6 +154,7 @@ uint64_t CalculateCurrentUsage()
 /* Prune a block file (modify associated database entries)*/
 void PruneOneBlockFile(const int fileNumber)
 {
+    READLOCK(cs_mapBlockIndex);
     for (BlockMap::iterator it = mapBlockIndex.begin(); it != mapBlockIndex.end(); ++it)
     {
         CBlockIndex *pindex = it->second;

src/checkpoints.cpp
@@ -68,9 +68,10 @@ int GetTotalBlocksEstimate(const CCheckpointData &data)
     return checkpoints.rbegin()->first;
 }
 
+#if 0 // UNUSED
 CBlockIndex *GetLastCheckpoint(const CCheckpointData &data)
 {
-    AssertLockHeld(cs_main); // for mapBlockIndex
+    AssertLockHeld(cs_mapBlockIndex);
     const MapCheckpoints &checkpoints = data.mapCheckpoints;
     for (auto i = checkpoints.rbegin(); i != checkpoints.rend(); i++)
     {
@@ -81,5 +82,5 @@ CBlockIndex *GetLastCheckpoint(const CCheckpointData &data)
     }
     return NULL;
 }
-
+#endif
 } // namespace Checkpoints

src/checkpoints.h
@@ -22,8 +22,8 @@ namespace Checkpoints
 //! Return conservative estimate of total number of blocks, 0 if unknown
 int GetTotalBlocksEstimate(const CCheckpointData &data);
 
-//! Returns last CBlockIndex* in mapBlockIndex that is a checkpoint
-CBlockIndex *GetLastCheckpoint(const CCheckpointData &data);
+//! Unused: Returns last CBlockIndex* in mapBlockIndex that is a checkpoint
+// CBlockIndex *GetLastCheckpoint(const CCheckpointData &data);
 
 double GuessVerificationProgress(const CCheckpointData &data, CBlockIndex *pindex, bool fSigchecks = true);
 

src/consensus/tx_verify.cpp
@@ -218,7 +218,7 @@ bool CheckTransaction(const CTransaction &tx, CValidationState &state)
  */
 static int GetSpendHeight(const CCoinsViewCache &inputs)
 {
-    LOCK(cs_main);
+    READLOCK(cs_mapBlockIndex);
     BlockMap::iterator i = mapBlockIndex.find(inputs.GetBestBlock());
     if (i != mapBlockIndex.end())
     {

commit 0c7f13cb22bd834d71f4cdf597e1713d44683f48

fix logic issue introduced in #1487

src/blockstorage/blockstorage.cpp
@@ -357,7 +357,7 @@ void SyncStorage(const CChainParams &chainparams)
             }
 
             index = LookupBlockIndex(item.second.GetBlockHash());
-            if (index)
+            if (!index)
             {
                 CBlockIndex *pindexNew = InsertBlockIndex(item.second.GetBlockHash());
                 pindexNew->pprev = InsertBlockIndex(item.second.hashPrev);