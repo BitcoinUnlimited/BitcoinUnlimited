cd0fd1df7deeb0f4860ca09feb3e03240978fbac
Andrea Suisani <sickpig@gmail.com>
2019-02-14 09:17:44 +0100

    Removed unused patches from qt.mk

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -8,9 +8,7 @@ $(package)_dependencies=openssl zlib
 $(package)_linux_dependencies=freetype fontconfig libxcb libX11 xproto libXext
 $(package)_build_subdir=qtbase
 $(package)_qt_libs=corelib network widgets gui plugins testlib
-$(package)_patches=mac-qmake.conf mingw-uuidof.patch pidlist_absolute.patch fix-xcb-include-order.patch
-$(package)_patches+=fix_qt_pkgconfig.patch fix-cocoahelpers-macos.patch qfixed-coretext.patch fix_qt571_win32_qmake.patch
-$(package)_patches+=fix_configure_mac.patch fix_no_printer.patch fix_rcc_determinism.patch xkb-default.patch
+$(package)_patches=mac-qmake.conf fix_qt_pkgconfig.patch fix_qt571_win32_qmake.patch fix_configure_mac.patch fix_no_printer.patch fix_rcc_determinism.patch xkb-default.patch
 
 $(package)_qttranslations_file_name=qttranslations-$($(package)_suffix)
 $(package)_qttranslations_sha256_hash=b36da7d93c3ab6fca56b32053bb73bc619c8b192bb89b74e3bcde2705f1c2a14
