33d3bb257e82d4daddf28fd6e135fdcd7443bd79
Andrea Suisani <sickpig@gmail.com>
2019-04-09 09:51:55 +0200

    Add an env var to disalble dependencies building in travis

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -15,4 +15,6 @@ if [ -n "$OSX_SDK" -a ! -f depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz ]; th
 if [ -n "$OSX_SDK" -a -f depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz ]; then tar -C depends/SDKs -xf depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz; fi
 echo "USE_CLANG=$USE_CLANG"
 if [[ $HOST = *-mingw32 ]]; then DOCKER_EXEC update-alternatives --set $HOST-g++ \$\(which $HOST-g++-posix\); fi
-if [ "$USE_CLANG" = "false" ]; then DOCKER_EXEC CONFIG_SHELL= make $MAKEJOBS -C depends HOST=$HOST $DEP_OPTS; fi #use sys libraries for clang
+if [ -z "$NODEPENDS" ]; then
+  if [ "$USE_CLANG" = "false" ]; then DOCKER_EXEC CONFIG_SHELL= make $MAKEJOBS -C depends HOST=$HOST $DEP_OPTS; fi #use sys libraries for clang
+fi
