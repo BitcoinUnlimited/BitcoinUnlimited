9577b373a0204cfed03c630386cc9318b6c09d47
Marius Kjærstad <sandakersmann@users.noreply.github.com>
2019-05-28 16:06:51 +0200

    Update COPYRIGHT_YEAR in clientversion.h to 2019 (#1759)
    Update COPYRIGHT_YEAR in clientversion.h to 2019

--- src/clientversion.h
+++ src/clientversion.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2009-2015 The Bitcoin Core developers
-// Copyright (c) 2015-2018 The Bitcoin Unlimited developers
+// Copyright (c) 2015-2019 The Bitcoin Unlimited developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -27,7 +27,7 @@
  * Copyright year (2009-this)
  * Todo: update this when changing our copyright comments in the source
  */
-#define COPYRIGHT_YEAR 2018
+#define COPYRIGHT_YEAR 2019
 
 #endif // HAVE_CONFIG_H
 
