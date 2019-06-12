36fae130f005a074e4861ede4b22acf40b691638
Andrea Suisani <sickpig@gmail.com>
2019-06-12 18:13:23 +0200

    [travis] fix unit tests execution
    We used to execute unit tests (make check) twice, both in script_a.sh
    and script_b.sh. Removed 'make check' execution from the former.

--- .travis/script_b.sh
+++ .travis/script_b.sh
@@ -13,7 +13,7 @@ cd "build" || (echo "could not enter distdir build"; exit 1)
 
 BEGIN_FOLD unit-tests
 if [ "$RUN_TESTS" = "true" ] && ! { [ "$HOST" = "i686-w64-mingw32" ] || [ "$HOST" = "x86_64-w64-mingw32" ]; }; then
-  DOCKER_EXEC LD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/depends/$HOST/lib make $MAKEJOBS check VERBOSE=1;
+  travis_wait 50 DOCKER_EXEC LD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/depends/$HOST/lib make $MAKEJOBS check VERBOSE=1;
 fi
 END_FOLD
 
