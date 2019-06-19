a9a83d152ca6318f63487df8638a86e1bc37aebb
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2019-06-19 08:52:02 +0200

    Bump electrs to v0.7.0

--- contrib/electrs/build_electrs.py
+++ contrib/electrs/build_electrs.py
@@ -5,8 +5,8 @@ import os
 import sys
 import shutil
 GIT_REPO = "https://github.com/dagurval/electrs.git"
-GIT_BRANCH = "v0.6.1bu"
-EXPECT_HEAD = "175c1d161b5d30c60a20b5b9922e65fd05e6b59c"
+GIT_BRANCH = "v0.7.0bu"
+EXPECT_HEAD = "5d37e7fe52e5cd81c00f269ad09dd32a835ff808"
 
 ROOT_DIR = os.path.realpath(
         os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
