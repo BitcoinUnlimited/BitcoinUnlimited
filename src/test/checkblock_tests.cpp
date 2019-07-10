BitcoinUnlimited/BitcoinUnlimited

commit c64d44b7461cbe952a63d20141cff7f035fdaae7

clean up merge of new opcodes, add SV config and implement max instruction increase.  Add a config validator for the forks.  To do so, it was necessary to extend the CTweak to support validators like CTweakRef currently does.  Shorten QA test time

qa/pull-tester/rpc-tests.py
@@ -191,7 +191,6 @@ testScripts = [ RpcTest(t) for t in [
     'notify',
     Disabled('may152018_forkactivation_1','May 2018 already activated, use it as template to test future upgrade activation'),
     Disabled('may152018_forkactivation_2','May 2018 already activated, use it as template to test future upgrade activation'),
-    'bip68-112-113-p2p',
     'validateblocktemplate',
     'parallel',
     'wallet',
@@ -218,7 +217,6 @@ testScripts = [ RpcTest(t) for t in [
     'merkle_blocks',
     'fundrawtransaction',
     'signrawtransactions',
-    'walletbackup',
     'nodehandling',
     'reindex',
     'decodescript',
@@ -238,6 +236,8 @@ testScripts = [ RpcTest(t) for t in [
 ] ]
 
 testScriptsExt = [ RpcTest(t) for t in [
+    'walletbackup',
+    'bip68-112-113-p2p',
     'limits',
     'weirdtx',
     'txPerf',

qa/rpc-tests/ctor.py
@@ -122,27 +122,27 @@ class MyTest (BitcoinTestFramework):
         # dtor rollback test
         disconnect_all(self.nodes[1])
         disconnect_all(self.nodes[0])
-        for j in range(10):
+        for j in range(5):
             for i in range(3):
                 self.nodes[1].sendtoaddress(addr[1], 4-i)
             self.nodes[1].generate(1)
 
-        for j in range(9):
+        for j in range(4):
             for i in range(3):
                 self.nodes[0].sendtoaddress(addr[0], 4-i)
             self.nodes[0].generate(1)
         connect_nodes(self.nodes[0], 1)
-        
+
         waitFor(10, lambda: self.nodes[0].getbestblockhash() == self.nodes[1].getbestblockhash())
 
         # ctor rollback test
         disconnect_all(self.nodes[3])
-        for j in range(10):
+        for j in range(5):
             for i in range(3):
                 self.nodes[3].sendtoaddress(addr[3], 4-i)
             self.nodes[3].generate(1)
 
-        for j in range(9):
+        for j in range(4):
             for i in range(3):
                 self.nodes[2].sendtoaddress(addr[2], 4-i)
             self.nodes[2].generate(1)
@@ -152,9 +152,8 @@ class MyTest (BitcoinTestFramework):
         #print(self.nodes[2].getchaintips())
         waitFor(10, lambda: self.nodes[2].getbestblockhash() == self.nodes[3].getbestblockhash())
 
-        
         # push the dtor chain beyond ctor
-        for i in range(0,50):
+        for i in range(0,30):
             self.nodes[0].sendtoaddress(addr[0], 1)
             self.nodes[0].generate(1)
 
@@ -163,7 +162,7 @@ class MyTest (BitcoinTestFramework):
         ctorTipHeight = self.nodes[3].getblockcount()
         dtorTip = self.nodes[0].getbestblockhash()
         dtorTipHeight = self.nodes[0].getblockcount()
-        
+
         self.nodes.append(start_node(4, self.options.tmpdir))
         self.nodes[4].set(FORK_CFG + "=0", "consensus.enableCanonicalTxOrder=1")
         for i in range(5):
@@ -185,7 +184,7 @@ class MyTest (BitcoinTestFramework):
 
         # make the new fork longer than current
         for n in range(10):
-            time.sleep(1)
+            time.sleep(.1)
             rollbackNode.generate(1)
 
         preFork = rollbackNode.generate(1)[0] # will be a dtor block

qa/rpc-tests/excessive.py
@@ -79,10 +79,6 @@ class ExcessiveBlockTest (BitcoinTestFramework):
         self.testExcessiveSigops()
 
         # clear out the mempool
-        mostly_sync_mempools(self.nodes)
-        for n in self.nodes:
-            n.generate(2)
-            sync_blocks(self.nodes)
         for n in self.nodes:
             while len(n.getrawmempool()):
                 n.generate(1)
@@ -155,10 +151,14 @@ class ExcessiveBlockTest (BitcoinTestFramework):
             counts = [x.getblockcount() for x in self.nodes]
             if counts == blockHeights:
                 return True  # success!
-            time.sleep(1)
-            loop += 1
-            if ((loop % 30) == 0):
-                logging.info("...waiting %s" % loop)
+            else:
+                for (a,b) in zip(counts, blockHeights):
+                    if counts > blockHeights:
+                        assert("blockchain synced too far")
+            time.sleep(.25)
+            loop += .25
+            if int(loop) == loop and (int(loop) % 10) == 0:
+                logging.info("...waiting %f %s != %s" % (loop, counts, blockHeights))
         return False
 
     def generateTx(self, node, txBytes, addrs, data=None):
@@ -196,28 +196,21 @@ class ExcessiveBlockTest (BitcoinTestFramework):
     def testExcessiveSigops(self):
         """This test checks the behavior of the nodes in the presence of transactions that take a long time to validate.
         """
-        NUM_ADDRS = 100
+        NUM_ADDRS = 20
         logging.info("testExcessiveSigops: Cleaning up node state")
 
         # We are not testing excessively sized blocks so make these large
-        for i in range(0,3):
+        for i in range(0,4):
             self.nodes[i].setminingmaxblock(5000000)
             self.nodes[i].set("net.excessiveBlock=5000000")
             # Stagger the accept depths so we can see the block accepted stepwise
             self.nodes[i].set("net.excessiveAcceptDepth=%d"%(i))
 
-        for n in self.nodes:
-            n.generate(10)
-            self.sync_blocks()
-
-        self.nodes[0].generate(100)  # create a lot of BTC for spending
+        self.nodes[0].generate(20)  # create a lot of BTC for spending
 
         self.sync_blocks()
 
-        self.nodes[0].set("net.excessiveSigopsPerMb=100")  # Set low so txns will fail if its used
-        self.nodes[1].set("net.excessiveSigopsPerMb=5000")
-        self.nodes[2].set("net.excessiveSigopsPerMb=1000")
-        self.nodes[3].set("net.excessiveSigopsPerMb=100")
+        self.nodes[0].set("net.excessiveSigopsPerMb=19")  # Set low so txns will fail if its used
 
         logging.info("Creating addresses...")
         self.nodes[0].keypoolrefill(NUM_ADDRS)
@@ -243,6 +236,9 @@ class ExcessiveBlockTest (BitcoinTestFramework):
             wallet = self.nodes[0].listunspent()
             wallet.sort(key=lambda x: x["amount"], reverse=True)
             self.nodes[0].set("net.excessiveSigopsPerMb=100000")  # Set this huge so all txns are accepted by this node
+            self.nodes[1].set("net.excessiveSigopsPerMb=10")
+            self.nodes[2].set("net.excessiveSigopsPerMb=10")
+            self.nodes[3].set("net.excessiveSigopsPerMb=10")
 
             logging.info("Generate > 1MB block with excessive sigops")
             self.generateTx(self.nodes[0], 1100000, addrs)
@@ -256,23 +252,23 @@ class ExcessiveBlockTest (BitcoinTestFramework):
             logging.info("Test excessive block propagation to nodes with different AD")
             self.nodes[0].generate(1)
             # it takes a while to sync all the txns
-            assert_equal(True, self.expectHeights([base + 2, base + 2, base, base], 500))
+            assert_equal(True, self.expectHeights([base + 2, base + 2, base, base], 30))
 
             self.nodes[0].generate(1)
-            assert_equal(True, self.expectHeights([base + 3, base + 3, base + 3, base], 90))
+            assert_equal(True, self.expectHeights([base + 3, base + 3, base + 3, base], 30))
 
             self.nodes[0].generate(1)
-            assert_equal(True, self.expectHeights([base + 4, base + 4, base + 4, base + 4], 90))
+            assert_equal(True, self.expectHeights([base + 4, base + 4, base + 4, base + 4], 30))
+
+            # Reset the excessive block logic by mining a day worth of blocks
+            for n in self.nodes:
+                n.generate(int(144/len(self.nodes) + 1))
+                self.sync_blocks()
 
         logging.info("Excessive sigops test completed")
 
         # set it all back to defaults
-
-        for n in self.nodes:
-            n.generate(150)
-            self.sync_blocks()
-
-        self.nodes[0].set("net.excessiveSigopsPerMb=20000")  # Set low so txns will fail if its used
+        self.nodes[0].set("net.excessiveSigopsPerMb=20000")
         self.nodes[1].set("net.excessiveSigopsPerMb=20000")
         self.nodes[2].set("net.excessiveSigopsPerMb=20000")
         self.nodes[3].set("net.excessiveSigopsPerMb=20000")
@@ -285,6 +281,7 @@ class ExcessiveBlockTest (BitcoinTestFramework):
         self.nodes[1].set("net.excessiveBlock=1000000")
         self.nodes[2].set("net.excessiveBlock=1000000")
         self.nodes[3].set("net.excessiveBlock=1000000")
+
 
     def testExcessiveTx(self):
         """This test checks the behavior of the nodes in the presence of excessively large transactions.
@@ -331,10 +328,10 @@ class ExcessiveBlockTest (BitcoinTestFramework):
             if 1:  # Test not relaying a large transaction
 
                 # Make the excessive transaction size smaller so its quicker to produce a excessive one
-                self.nodes[0].set("net.excessiveTx=100000")
-                self.nodes[1].set("net.excessiveTx=100000")
-                self.nodes[2].set("net.excessiveTx=100000")
-                self.nodes[3].set("net.excessiveTx=100000")
+                self.nodes[0].set("net.excessiveTx=10000")
+                self.nodes[1].set("net.excessiveTx=10000")
+                self.nodes[2].set("net.excessiveTx=10000")
+                self.nodes[3].set("net.excessiveTx=10000")
                 self.nodes[0].setminingmaxblock(1000000)
                 self.nodes[1].setminingmaxblock(1000000)
                 self.nodes[2].setminingmaxblock(1000000)
@@ -342,7 +339,7 @@ class ExcessiveBlockTest (BitcoinTestFramework):
 
                 wallet = self.nodes[0].listunspent()
                 wallet.sort(key=lambda x: x["amount"], reverse=True)
-                while len(wallet) < 3000:
+                while len(wallet) < 500:
                     # Create a LOT of UTXOs
                     logging.info("Create lots of UTXOs...")
                     n = 0
@@ -377,9 +374,9 @@ class ExcessiveBlockTest (BitcoinTestFramework):
 
                 logging.info("Test not relaying a large transaction")
 
-                (tx, vin, vout, txid) = split_transaction(self.nodes[0], wallet[0:3000], [addrs[0]], txfeePer=60)
+                (tx, vin, vout, txid) = split_transaction(self.nodes[0], wallet[0:500], [addrs[0]], txfeePer=60)
                 logging.debug("Transaction Length is: ", len(binascii.unhexlify(tx)))
-                assert(len(binascii.unhexlify(tx)) > 100000)  # txn has to be big for the test to work
+                assert(len(binascii.unhexlify(tx)) > 10000)  # txn has to be big for the test to work
 
                 mbefore = [(lambda y: (y["size"], y["bytes"]))(x.getmempoolinfo()) for x in self.nodes]
                 assert_equal(mbefore[1:], [(0, 0), (0, 0), (0, 0)])  # verify that the transaction did not propagate
@@ -389,7 +386,6 @@ class ExcessiveBlockTest (BitcoinTestFramework):
                 while len(self.nodes[0].getmempoolinfo()) < 1:
                     logging.info("sleeping 1")
                     time.sleep(1)
-                time.sleep(5)
 
                 logging.info("Test a large transaction in block < 1MB")
                 largeBlock = self.nodes[0].generate(1)
@@ -503,8 +499,6 @@ class ExcessiveBlockTest (BitcoinTestFramework):
                 self.sync_all()
             self.nodes[0].generate(100)
 
-        self.sync_blocks()
-
         # Set the accept depth at 1, 2, and 3 and watch each nodes resist the chain for that long
         self.nodes[0].setminingmaxblock(5000)  # keep the generated blocks within 16*the EB so no disconnects
         self.nodes[1].setminingmaxblock(1000)
@@ -527,36 +521,26 @@ class ExcessiveBlockTest (BitcoinTestFramework):
         logging.info("Starting counts: %s" % str(counts))
         logging.info("node0")
         self.generateAndPrintBlock(self.nodes[0])
-        time.sleep(2)  # give blocks a chance to fully propagate
-        counts = [x.getblockcount() for x in self.nodes]
-        assert_equal(counts, [base + 1, base, base, base])
+        assert_equal(True, self.expectHeights([base + 1, base, base, base]))
 
         logging.info("node1")
         self.nodes[0].generate(1)
-        sync_blocks(self.nodes[0:2])
-        counts = [x.getblockcount() for x in self.nodes]
-        assert_equal(counts, [base + 2, base + 2, base, base])
+        assert_equal(True, self.expectHeights([base + 2, base + 2, base, base]))
 
         logging.info("node2")
         self.nodes[0].generate(1)
-        sync_blocks(self.nodes[0:3])
-        counts = [x.getblockcount() for x in self.nodes]
-        assert_equal(counts, [base + 3, base + 3, base + 3, base])
+        assert_equal(True, self.expectHeights([base + 3, base + 3, base + 3, base]))
 
         logging.info("node3")
         self.nodes[0].generate(1)
-        self.sync_blocks()
-        counts = [x.getblockcount() for x in self.nodes]
-        assert_equal(counts, [base + 4] * 4)
+        assert_equal(True, self.expectHeights([base + 4] * 4))
 
         # Now generate another excessive block, but all nodes should snap right to
         # it because they have an older excessive block
         logging.info("Test immediate propagation of additional excessively sized block, due to prior excessive")
         self.repeatTx(8, self.nodes[0], addr, .001)
         self.nodes[0].generate(1)
-        self.sync_blocks()
-        counts = [x.getblockcount() for x in self.nodes]
-        assert_equal(counts, [base + 5] * 4)
+        assert_equal(True, self.expectHeights([base + 5] * 4))
 
         logging.info("Test daily excessive reset")
         # Now generate a day's worth of small blocks which should re-enable the
@@ -598,29 +582,30 @@ class ExcessiveBlockTest (BitcoinTestFramework):
         counts = [x.getblockcount() for x in self.nodes]
         assert_equal(counts, [base + 5] * 4)
 
-        logging.info("Test daily excessive reset #2")
-        # Now generate a day's worth of small blocks which should re-enable the
-        # node's reluctance to accept a large block + 10 because we have to get
-        # beyond all the node's accept depths
-        self.nodes[0].generate(6 * 24 + 10)
-        sync_blocks(self.nodes)
-
-        # counts = [ x.getblockcount() for x in self.nodes ]
-        self.nodes[1].setexcessiveblock(100000, 1)  # not sure how big the txns will be but smaller than this
-        self.nodes[1].setminingmaxblock(100000)  # not sure how big the txns will be but smaller than this
-        self.repeatTx(20, self.nodes[0], addr, .001)
-        base = self.nodes[0].getblockcount()
-        self.generateAndPrintBlock(self.nodes[0])
-        time.sleep(2)  # give blocks a chance to fully propagate
-        sync_blocks(self.nodes[0:2])
-        counts = [x.getblockcount() for x in self.nodes]
-        assert_equal(counts, [base + 1, base + 1, base, base])
-
-        logging.info("Random test")
         if self.extended:
+            logging.info("Test daily excessive reset #2")
+            # Now generate a day's worth of small blocks which should re-enable the
+            # node's reluctance to accept a large block + 10 because we have to get
+            # beyond all the node's accept depths
+            self.nodes[0].generate(6 * 24 + 10)
+            sync_blocks(self.nodes)
+
+            # counts = [ x.getblockcount() for x in self.nodes ]
+            self.nodes[1].setexcessiveblock(100000, 1)  # not sure how big the txns will be but smaller than this
+            self.nodes[1].setminingmaxblock(100000)  # not sure how big the txns will be but smaller than this
+            self.repeatTx(20, self.nodes[0], addr, .001)
+            base = self.nodes[0].getblockcount()
+            self.generateAndPrintBlock(self.nodes[0])
+            time.sleep(2)  # give blocks a chance to fully propagate
+            sync_blocks(self.nodes[0:2])
+            counts = [x.getblockcount() for x in self.nodes]
+            assert_equal(counts, [base + 1, base + 1, base, base])
+
+        if self.extended:
+            logging.info("Random test")
             randomRange = 3
         else:
-            randomRange = 2
+            randomRange = 0
 
         for i in range(0, randomRange):
             logging.info("round %d" % i)
@@ -691,8 +676,8 @@ def info(type, value, tb):
 sys.excepthook = info
 
 def Test():
-    t = ExcessiveBlockTest(True)
-    # t.drop_to_pdb = True
+    t = ExcessiveBlockTest()
+    t.drop_to_pdb = True
     bitcoinConf = {
         "debug": ["rpc", "net", "blk", "thin", "mempool", "req", "bench", "evict"],
         "blockprioritysize": 2000000,  # we don't want any transactions rejected due to insufficient fees...

qa/rpc-tests/fundrawtransaction.py
@@ -25,7 +25,7 @@ class RawTransactionsTest(BitcoinTestFramework):
         connect_nodes_bi(self.nodes,0,3)
 
         self.is_network_split=False
-        self.sync_all()
+        self.sync_blocks()
 
     def run_test(self):
         print("Mining blocks...")
@@ -47,9 +47,9 @@ class RawTransactionsTest(BitcoinTestFramework):
             feeTolerance = 0.00000001
 
         self.nodes[2].generate(1)
-        self.sync_all()
+        self.sync_blocks()
         self.nodes[0].generate(121)
-        self.sync_all()
+        self.sync_blocks()
 
         watchonly_address = self.nodes[0].getnewaddress()
         watchonly_pubkey = self.nodes[0].validateaddress(watchonly_address)["pubkey"]
@@ -62,9 +62,8 @@ class RawTransactionsTest(BitcoinTestFramework):
         self.nodes[0].sendtoaddress(self.nodes[2].getnewaddress(), 1.0)
         self.nodes[0].sendtoaddress(self.nodes[2].getnewaddress(), 5.0)
 
-        self.sync_all()
         self.nodes[0].generate(1)
-        self.sync_all()
+        self.sync_blocks()
 
         ###############
         # simple test #

qa/rpc-tests/mempool_accept.py
@@ -212,6 +212,7 @@ class MyTest (BitcoinTestFramework):
     def conflictTest(self, dests0, dests1, wallet, numNodes=2):
         """Tests issuing a bunch of conflicting transactions.  Expects that you give it a wallet with lots of free UTXO, and nothing in the mempool
         """
+        logging.info("conflict test")
         assert(self.nodes[0].getmempoolinfo()["size"] == 0)  # Expects a clean mempool
         if 1:  # test many conflicts
             NTX = 50
@@ -227,7 +228,7 @@ class MyTest (BitcoinTestFramework):
 
             for n in self.nodes:
                 waitFor(30, lambda: True if n.getmempoolinfo()["size"] >= NTX - 5 else None)
-            time.sleep(5)
+            time.sleep(1)
             # we have to allow < because bloom filter false positives in the node's
             # sending logic may cause it to not get an INV
             for n in self.nodes:
@@ -235,7 +236,7 @@ class MyTest (BitcoinTestFramework):
             self.commitMempool()  # clear out this test
 
         if 1:  # test 2 conflicting transactions
-            NTX = 100
+            NTX = 25
             wallet2 = []
             gtx2 = []
             amt = createTx(dests0, wallet[0:NTX], 1, NTX, 1, wallet2, gtx2)
@@ -259,7 +260,7 @@ class MyTest (BitcoinTestFramework):
 
             waitFor(30, lambda: True if self.nodes[0].getmempoolinfo()["size"] >= NTX else None)
             waitFor(30, lambda: True if self.nodes[1].getmempoolinfo()["size"] >= NTX else None)
-            time.sleep(4)  # see if any conflicts will be added to the mempool
+            time.sleep(2)  # see if any conflicts will be added to the mempool
 
             assert(self.nodes[0].getmempoolinfo()["size"] == NTX)
             assert(self.nodes[1].getmempoolinfo()["size"] == NTX)
@@ -267,7 +268,7 @@ class MyTest (BitcoinTestFramework):
             NTX1 = NTX
 
             # test conflicting tx sent to different nodes
-            NTX = 200
+            NTX = 50
             wallet2 = []
             gtx2 = []
             amt = createTx(dests0, wallet[0:NTX], 1, NTX, 1, wallet2, gtx2)
@@ -294,13 +295,13 @@ class MyTest (BitcoinTestFramework):
             # complete
             waitFor(30, lambda: True if self.nodes[0].getmempoolinfo()["size"] >= NTX + NTX1 else None)
             waitFor(30, lambda: True if self.nodes[1].getmempoolinfo()["size"] >= NTX + NTX1 else None)
-
-            time.sleep(4)  # see if any conflicts will be added to the mempool
+            time.sleep(2)  # see if any conflicts will be added to the mempool
             assert(self.nodes[0].getmempoolinfo()["size"] == NTX + NTX1)
             assert(self.nodes[1].getmempoolinfo()["size"] == NTX + NTX1)
 
             # forget about the tx I used
             wallet = wallet[NTX:]
+            logging.info("conflict test done")
 
     def commitMempool(self):
         """Commit all the tx in mempools on all nodes into blocks"""
@@ -313,7 +314,7 @@ class MyTest (BitcoinTestFramework):
         decContext = decimal.getcontext().prec
         decimal.getcontext().prec = 8 + 8  # 8 digits to get to 21million, and each bitcoin is 100 million satoshis
 
-        self.nodes[0].generate(200)
+        self.nodes[0].generate(152)
         self.sync_blocks()
 
         # Get some addresses
@@ -321,7 +322,7 @@ class MyTest (BitcoinTestFramework):
         dests0 = [PayDest(self.nodes[0]) for x in range(20)]
 
         # Create 51 transaction and ensure that they get synced
-        NTX = 101
+        NTX = 51
         (amt, wallet) = self.threadedCreateTx(dests1, None, 0, NTX)
         assert(amt == NTX)
         waitFor(10, lambda: True if self.nodes[0].getmempoolinfo()["size"] >= NTX else None)
@@ -336,7 +337,7 @@ class MyTest (BitcoinTestFramework):
         self.commitMempool()
 
         # Create 500 transaction and ensure that they get synced
-        NTX = 500
+        NTX = 500 if self.bigTest else 100
         start = time.monotonic()
         (amt, wallet) = self.threadedCreateTx(dests0, wallet, 1, NTX)
         end = time.monotonic()
@@ -392,24 +393,25 @@ class MyTest (BitcoinTestFramework):
         end = time.monotonic()
         logging.info("synced %d tx in %s seconds.  Speed %f tx/sec" % (NTX, end - start, float(NTX) / (end - start)))
 
-        # Start up node 4
-        self.nodes.append(start_node(3, self.options.tmpdir))
-        connect_nodes_bi(self.nodes, 0, 3)
-        connect_nodes_bi(self.nodes, 1, 3)
-        connect_nodes_bi(self.nodes, 2, 3)
-        sync_blocks(self.nodes)
+        if self.bigTest:
+            # Start up node 4
+            self.nodes.append(start_node(3, self.options.tmpdir))
+            connect_nodes_bi(self.nodes, 0, 3)
+            connect_nodes_bi(self.nodes, 1, 3)
+            connect_nodes_bi(self.nodes, 2, 3)
+            sync_blocks(self.nodes)
 
-        # Push all tx to node 4 from many nodes
-        destName = "127.0.0.1:" + str(p2p_port(3))
+            # Push all tx to node 4 from many nodes
+            destName = "127.0.0.1:" + str(p2p_port(3))
 
-        start = time.monotonic()
-        self.nodes[0].pushtx(destName)
-        self.nodes[1].pushtx(destName)
-        self.nodes[2].pushtx(destName)
-        mp = waitFor(120, lambda: [x.getmempoolinfo() for x in self.nodes]
-                     if NTX - self.nodes[3].getmempoolinfo()["size"] < 30 else None)
-        end = time.monotonic()
-        logging.info("synced %d tx in %s seconds.  Speed %f tx/sec" % (NTX, end - start, float(NTX) / (end - start)))
+            start = time.monotonic()
+            self.nodes[0].pushtx(destName)
+            self.nodes[1].pushtx(destName)
+            self.nodes[2].pushtx(destName)
+            mp = waitFor(120, lambda: [x.getmempoolinfo() for x in self.nodes]
+                         if NTX - self.nodes[3].getmempoolinfo()["size"] < 30 else None)
+            end = time.monotonic()
+            logging.info("synced %d tx in %s seconds.  Speed %f tx/sec" % (NTX, end - start, float(NTX) / (end - start)))
 
 
 if __name__ == '__main__':

qa/rpc-tests/nov152018_forkactivation.py
@@ -6,9 +6,10 @@
 This test checks that simple features of the magnetic anomaly fork
 activates properly. More complex features are given their own tests.
 """
+import os
 
 from test_framework.test_framework import ComparisonTestFramework
-from test_framework.util import assert_equal, assert_raises_rpc_error
+from test_framework.util import assert_equal, assert_raises_rpc_error, findBitcoind, JSONRPCException
 from test_framework.comptool import TestManager, TestInstance, RejectResult
 from test_framework.blocktools import create_coinbase, create_block
 from test_framework.mininode import *
@@ -38,6 +39,27 @@ class MagneticAnomalyActivationTest(ComparisonTestFramework):
                             "-consensus.forkNov2018Time=%d" % NOV152018_START_TIME]]
 
     def run_test(self):
+
+        # Check configuration settings
+        self.nodes[0].set("consensus.forkNov2018Time=1")
+        assert(self.nodes[0].get("consensus.forkNov2018Time")["consensus.forkNov2018Time"] == 1542300000)
+        try:
+            self.nodes[0].set("consensus.svForkNov2018Time=1")
+            assert(0)  # should have thrown exception, can't set both ABC and SV forks at the same time
+        except JSONRPCException as e:
+            pass
+        # now set SV fork & check the default
+        self.nodes[0].set("consensus.forkNov2018Time=0")
+        self.nodes[0].set("consensus.svForkNov2018Time=1")
+        assert(self.nodes[0].get("consensus.svForkNov2018Time")["consensus.svForkNov2018Time"] == 1542300000)
+        subver = self.nodes[0].getnetworkinfo()["subversion"]
+        assert("SV" in subver)
+
+        self.nodes[0].set("consensus.svForkNov2018Time=0")
+        subver = self.nodes[0].getnetworkinfo()["subversion"]
+        assert(not "SV" in subver)
+
+        # Run TestManager based tests
         self.test = TestManager(self, self.options.tmpdir)
         self.test.add_all_connections(self.nodes)
         # Start up network handling in another thread

commit 3e7f8d68e780704861ec25a72d6440fb753bb5ae

Fix checkblock_test hidden failure
FIX: Test will now fail if "testblock.dat" fails to load.  Previously this caused a false-positive for this test.
FIX: Define TEST_DATA_DIR in makefile so path to testblock.dat is correct.
FIX: Actual test failure caused by c64d44b7 move set of fExcessive from CheckBlock to ContextualCheckBlock.  This failure was hidden by the first issue of false-positives when "testblock.dat" failed to load.

src/Makefile.test.include
@@ -153,7 +153,7 @@ test_test_bitcoin_SOURCES = $(BITCOIN_TEST_SUITE) $(BITCOIN_TESTS) $(JSON_TEST_F
 test_test_bitcoin_CPPFLAGS = $(AM_CPPFLAGS) $(BITCOIN_INCLUDES) -I$(builddir)/test/ $(TESTDEFS)
 test_test_bitcoin_LDADD = $(LIBBITCOIN_SERVER) $(LIBBITCOIN_CLI) $(LIBBITCOIN_COMMON) $(LIBBITCOIN_UTIL) $(LIBBITCOIN_CONSENSUS) $(LIBBITCOIN_CRYPTO) $(LIBUNIVALUE) \
   $(LIBLEVELDB) $(LIBLEVELDB_SSE42) $(LIBMEMENV) $(EVENT_LIBS) $(BOOST_LIBS) $(BOOST_UNIT_TEST_FRAMEWORK_LIB) $(LIBSECP256K1) $(LIBRSM)
-test_test_bitcoin_CXXFLAGS = $(AM_CXXFLAGS) $(PIE_FLAGS)
+test_test_bitcoin_CXXFLAGS = $(AM_CXXFLAGS) $(PIE_FLAGS) -DTEST_DATA_DIR=$(srcdir)/test/data/
 
 if ENABLE_WALLET
 test_test_bitcoin_LDADD += $(LIBBITCOIN_WALLET)

src/test/checkblock_tests.cpp
@@ -41,24 +41,36 @@ bool read_block(const std::string &filename, CBlock &block)
     return true;
 }
 
+bool LockAndContextualCheckBlock(CBlock &block, CValidationState &state)
+{
+    LOCK(cs_main);
+    return ContextualCheckBlock(block, state, nullptr, false);
+}
+
 BOOST_FIXTURE_TEST_SUITE(checkblock_tests, BasicTestingSetup) // BU harmonize suite name with filename
 
 
 BOOST_AUTO_TEST_CASE(TestBlock)
 {
     CBlock testblock;
-    if (read_block("testblock.dat", testblock))
+    bool fReadBlock = read_block("testblock.dat", testblock);
+    BOOST_CHECK_MESSAGE(fReadBlock, "Failed to read testblock.dat");
+    if (fReadBlock)
     {
         CValidationState state;
 
         uint64_t blockSize = ::GetSerializeSize(testblock, SER_NETWORK, PROTOCOL_VERSION); // 53298 B for test.dat
 
         BOOST_CHECK_MESSAGE(CheckBlock(testblock, state, false, false), "Basic CheckBlock failed");
+        // NOTE: setting of fExcessive was moved from CheckBlock to ContextualCheckBlock in c64d44b7
+        BOOST_CHECK_MESSAGE(LockAndContextualCheckBlock(testblock, state), "Contextual CheckBlock failed");
         BOOST_CHECK_MESSAGE(!testblock.fExcessive,
             "Block with size " << blockSize << " ought not to have been excessive when excessiveBlockSize is "
                                << excessiveBlockSize);
         excessiveBlockSize = blockSize - 1;
         BOOST_CHECK_MESSAGE(CheckBlock(testblock, state, false, false), "Basic CheckBlock failed");
+        // NOTE: setting of fExcessive was moved from CheckBlock to ContextualCheckBlock in c64d44b7
+        BOOST_CHECK_MESSAGE(LockAndContextualCheckBlock(testblock, state), "Contextual CheckBlock failed");
         BOOST_CHECK_MESSAGE(testblock.fExcessive,
             "Block with size " << blockSize << " ought to have been excessive when excessiveBlockSize is "
                                << excessiveBlockSize);