dfd5963887d9407686e214fe8d229785c6717c0f
Carl Dong <contact@carldong.me>
2019-07-17 15:43:25 -0400

    depends: libXext isn't needed by anyone
    libXext was only needed (as a library) by QT when it was using
    XLib/libX11 (as a library), now that we're building QT without
    XLib/libX11, we can safely remove libXext.

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -5,7 +5,7 @@ native_packages := native_ccache
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
-qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto xtrans
 
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
