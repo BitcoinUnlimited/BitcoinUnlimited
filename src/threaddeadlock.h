fb388e1a058917a1a85ffc92c52ee22853a872e5
Greg-Griffith <probablyaplebeian@protonmail.com>
2019-07-29 00:15:30 -0700

    add (empty) threaddeadlock files to be used for new deadlock detection

---
+++ src/threaddeadlock.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2019 Greg Griffith
+// Copyright (c) 2019 The Bitcoin Unlimited developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_THREAD_DEADLOCK_H
+#define BITCOIN_THREAD_DEADLOCK_H
+
+#endif
