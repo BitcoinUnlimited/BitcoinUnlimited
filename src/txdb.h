jimpo/bitcoin

[db] Migration for txindex data to new, separate database.

src/txdb.h
@@ -139,6 +139,10 @@ public:
 
     /// Read the best block hash of the chain that the txindex is in sync with.
     bool ReadBestBlockHash(uint256& hash) const;
+
+    /// Migrate txindex data from the block tree DB, where it may be for older nodes that have not
+    /// been upgraded yet to the new database.
+    bool MigrateData(CBlockTreeDB& block_tree_db, const uint256& block_hash);
 };
 
 #endif // BITCOIN_TXDB_H

BitcoinUnlimited/BitcoinUnlimited

src/txdb.h
@@ -220,5 +220,9 @@ public:
 
     /// Write block locator of the chain that the txindex is in sync with.
     bool WriteBestBlock(const CBlockLocator &locator);
+
+    /// Migrate txindex data from the block tree DB, where it may be for older nodes that have not
+    /// been upgraded yet to the new database.
+    bool MigrateData(CBlockTreeDB &block_tree_db, const CBlockLocator &best_locator);
 };
 #endif // BITCOIN_TXDB_H