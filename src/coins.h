0aabe85f1ba3c846531d93c70bccfd644e1c1db2
Peter Tschipper <peter.tschipper@gmail.com>
2019-08-08 13:22:24 -0700

    Document the in/out/return values for HaveCoinInCache()

--- src/coins.h
+++ src/coins.h
@@ -301,8 +301,11 @@ public:
 
     /**
      * Check if we have the given utxo already loaded in this cache.
-     * The semantics are the same as HaveCoin(), but no calls to
-     * the backing CCoinsView are made.
+     *
+     * @param[in]  outpoint   A reference to an outpoint in the coin we are checking
+     * @param[out] fSpent     A reference to an bool which indicates if the coin was spent or not
+     *                        NOTE: this value will not be set if the coin does not exist in cache.
+     * @return     bool       A return of true only indicates the coin is in cache, but not if it is spent/unspent
      */
     bool HaveCoinInCache(const COutPoint &outpoint, bool &fSpent) const;
 
