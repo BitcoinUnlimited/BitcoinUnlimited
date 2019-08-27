f79841996c8c01d187303873e5766d79a3498a07
Griffith <8345264+Greg-Griffith@users.noreply.github.com>
2019-08-28 01:55:11 +0900

    if not pruning, set preallocation of block/undo files to their max size (#1873)
    * if not pruning, set preallocation of block/undo files to their max size
    
    * move block file variables to blockstorage.cpp/.h

--- src/blockstorage/blockstorage.cpp
+++ src/blockstorage/blockstorage.cpp
@@ -4,7 +4,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "blockstorage/blockstorage.h"
+#include "blockstorage.h"
 
 #include "blockleveldb.h"
 #include "chainparams.h"
@@ -24,6 +24,8 @@ extern std::multimap<CBlockIndex *, CBlockIndex *> mapBlocksUnlinked;
 extern CTweak<uint64_t> pruneIntervalTweak;
 
 CDatabaseAbstract *pblockdb = nullptr;
+unsigned int blockfile_chunk_size = DEFAULT_BLOCKFILE_CHUNK_SIZE;
+unsigned int undofile_chunk_size = DEFAULT_UNDOFILE_CHUNK_SIZE;
 
 /**
   * Config param to determine what DB type we are using
@@ -843,22 +845,22 @@ bool FindBlockPos(CValidationState &state,
 
     if (!fKnown)
     {
-        unsigned int nOldChunks = (pos.nPos + BLOCKFILE_CHUNK_SIZE - 1) / BLOCKFILE_CHUNK_SIZE;
-        unsigned int nNewChunks = (vinfoBlockFile[nFile].nSize + BLOCKFILE_CHUNK_SIZE - 1) / BLOCKFILE_CHUNK_SIZE;
+        unsigned int nOldChunks = (pos.nPos + blockfile_chunk_size - 1) / blockfile_chunk_size;
+        unsigned int nNewChunks = (vinfoBlockFile[nFile].nSize + blockfile_chunk_size - 1) / blockfile_chunk_size;
         if (nNewChunks > nOldChunks)
         {
             if (fPruneMode)
             {
                 fCheckForPruning = true;
             }
-            if (CheckDiskSpace(nNewChunks * BLOCKFILE_CHUNK_SIZE - pos.nPos))
+            if (CheckDiskSpace(nNewChunks * blockfile_chunk_size - pos.nPos))
             {
                 FILE *file = OpenBlockFile(pos);
                 if (file)
                 {
-                    LOGA("Pre-allocating up to position 0x%x in blk%05u.dat\n", nNewChunks * BLOCKFILE_CHUNK_SIZE,
+                    LOGA("Pre-allocating up to position 0x%x in blk%05u.dat\n", nNewChunks * blockfile_chunk_size,
                         pos.nFile);
-                    AllocateFileRange(file, pos.nPos, nNewChunks * BLOCKFILE_CHUNK_SIZE - pos.nPos);
+                    AllocateFileRange(file, pos.nPos, nNewChunks * blockfile_chunk_size - pos.nPos);
                     fclose(file);
                 }
             }
@@ -893,22 +895,22 @@ bool FindUndoPos(CValidationState &state, int nFile, CDiskBlockPos &pos, unsigne
     nNewSize = vinfoBlockFile[nFile].nUndoSize += nAddSize;
     setDirtyFileInfo.insert(nFile);
 
-    unsigned int nOldChunks = (pos.nPos + UNDOFILE_CHUNK_SIZE - 1) / UNDOFILE_CHUNK_SIZE;
-    unsigned int nNewChunks = (nNewSize + UNDOFILE_CHUNK_SIZE - 1) / UNDOFILE_CHUNK_SIZE;
+    unsigned int nOldChunks = (pos.nPos + undofile_chunk_size - 1) / undofile_chunk_size;
+    unsigned int nNewChunks = (nNewSize + undofile_chunk_size - 1) / undofile_chunk_size;
     if (nNewChunks > nOldChunks)
     {
         if (fPruneMode)
         {
             fCheckForPruning = true;
         }
-        if (CheckDiskSpace(nNewChunks * UNDOFILE_CHUNK_SIZE - pos.nPos))
+        if (CheckDiskSpace(nNewChunks * undofile_chunk_size - pos.nPos))
         {
             FILE *file = OpenUndoFile(pos);
             if (file)
             {
                 LOGA(
-                    "Pre-allocating up to position 0x%x in rev%05u.dat\n", nNewChunks * UNDOFILE_CHUNK_SIZE, pos.nFile);
-                AllocateFileRange(file, pos.nPos, nNewChunks * UNDOFILE_CHUNK_SIZE - pos.nPos);
+                    "Pre-allocating up to position 0x%x in rev%05u.dat\n", nNewChunks * undofile_chunk_size, pos.nFile);
+                AllocateFileRange(file, pos.nPos, nNewChunks * undofile_chunk_size - pos.nPos);
                 fclose(file);
             }
         }
--- src/blockstorage/sequential_files.cpp
+++ src/blockstorage/sequential_files.cpp
@@ -6,7 +6,8 @@
 
 #include "sequential_files.h"
 
-#include "main.h"
+#include "blockstorage.h"
+
 
 extern bool AbortNode(CValidationState &state, const std::string &strMessage, const std::string &userMessage = "");
 extern bool fCheckForPruning;
@@ -195,7 +196,7 @@ void FindFilesToPruneSequential(std::set<int> &setFilesToPrune, uint64_t nLastBl
     // We don't check to prune until after we've allocated new space for files
     // So we should leave a buffer under our target to account for another allocation
     // before the next pruning.
-    uint64_t nBuffer = BLOCKFILE_CHUNK_SIZE + UNDOFILE_CHUNK_SIZE;
+    uint64_t nBuffer = blockfile_chunk_size + undofile_chunk_size;
     uint64_t nBytesToPrune;
     int count = 0;
 
