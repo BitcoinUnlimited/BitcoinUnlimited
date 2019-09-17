7a738f25e561f8c8b7a6b9de5ae087eece93d1a0
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2019-09-17 19:14:31 +0000

    Bump rust to 1.37.0

--- depends/packages/rust.mk
+++ depends/packages/rust.mk
@@ -1,8 +1,8 @@
 package=rust
-$(package)_version=1.32.0
+$(package)_version=1.37.0
 $(package)_download_path=https://static.rust-lang.org/dist
 $(package)_file_name=rust-$($(package)_version)-x86_64-unknown-linux-gnu.tar.gz
-$(package)_sha256_hash=e024698320d76b74daf0e6e71be3681a1e7923122e3ebd03673fcac3ecc23810
+$(package)_sha256_hash=cb573229bfd32928177c3835fdeb62d52da64806b844bc1095c6225b0665a1cb
 
 define $(package)_stage_cmds
   ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native --disable-ldconfig
