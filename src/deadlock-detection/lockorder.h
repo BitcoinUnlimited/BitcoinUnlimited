e6edf59d0565c37dbe80c930d00dc0fa763b7945
Peter Tschipper <peter.tschipper@gmail.com>
2019-09-27 13:22:41 -0700

    Fix lock guard definition in lockorder.h
    There was a type BITCOIN_LOCK_RODER --> should be BITCOIN_LOCK_ORDER

--- src/deadlock-detection/lockorder.h
+++ src/deadlock-detection/lockorder.h
@@ -4,7 +4,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef BITCOIN_LOCK_ORDER_H
-#define BITCOIN_LOCK_RODER_H
+#define BITCOIN_LOCK_ORDER_H
 
 #include <inttypes.h>
 #include <map>
