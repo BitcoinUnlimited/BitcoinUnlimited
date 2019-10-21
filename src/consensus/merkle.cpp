3902527ad515b5bb6f6baca2c2eed44f7601e7c4
Pieter Wuille <pieter.wuille@gmail.com>
2019-10-21 16:44:29 +0200

    Use SHA256D64 in Merkle root computation

--- src/consensus/merkle.cpp
+++ src/consensus/merkle.cpp
@@ -163,11 +163,31 @@ static void MerkleComputation(const std::vector<uint256> &leaves,
         *proot = h;
 }
 
-uint256 ComputeMerkleRoot(const std::vector<uint256> &leaves, bool *mutated)
+uint256 ComputeMerkleRoot(std::vector<uint256> hashes, bool *mutated)
 {
-    uint256 hash;
-    MerkleComputation(leaves, &hash, mutated, -1, nullptr);
-    return hash;
+    bool mutation = false;
+    while (hashes.size() > 1)
+    {
+        if (mutated)
+        {
+            for (size_t pos = 0; pos + 1 < hashes.size(); pos += 2)
+            {
+                if (hashes[pos] == hashes[pos + 1])
+                    mutation = true;
+            }
+        }
+        if (hashes.size() & 1)
+        {
+            hashes.push_back(hashes.back());
+        }
+        SHA256D64(hashes[0].begin(), hashes[0].begin(), hashes.size() / 2);
+        hashes.resize(hashes.size() / 2);
+    }
+    if (mutated)
+        *mutated = mutation;
+    if (hashes.size() == 0)
+        return uint256();
+    return hashes[0];
 }
 
 std::vector<uint256> ComputeMerkleBranch(const std::vector<uint256> &leaves, uint32_t position)
@@ -203,7 +223,7 @@ uint256 BlockMerkleRoot(const CBlock &block, bool *mutated)
     {
         leaves[s] = block.vtx[s]->GetHash();
     }
-    return ComputeMerkleRoot(leaves, mutated);
+    return ComputeMerkleRoot(std::move(leaves), mutated);
 }
 
 std::vector<uint256> BlockMerkleBranch(const CBlock &block, uint32_t position)
@@ -214,5 +234,5 @@ std::vector<uint256> BlockMerkleBranch(const CBlock &block, uint32_t position)
     {
         leaves[s] = block.vtx[s]->GetHash();
     }
-    return ComputeMerkleBranch(leaves, position);
+    return ComputeMerkleBranch(std::move(leaves), position);
 }
