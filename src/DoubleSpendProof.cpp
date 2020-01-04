fcb6069f7ef0eb275c3040d3544f8084b1a8f0a1
TomZ <tomz@freedommail.ch>
2020-01-04 14:16:37 +0100

    Import DoubleSpendProof support
    This takes the DoubleSpendProof functionality written by Tom Zander
    elsewhere and imports it into Bitcoin Unlimited, granting BU the ability
    to create, validate, store and propagate DSPs.
    
    Testing strategy;
    use a known regtest chain;
    https://gitlab.com/FloweeTheHub/thehub/blob/2b68d149/testing/api/blk00000.dat
    
    $ mkdir node1 node2
    copy the blk file into the `node1/regtest/blocks/` dir and make sure
    that the node using that dir will start with 'reindex'.
    
    $ bitcoin-qt -regtest -datadir=node1 -connect=localhost -printtoconsole=true \
        -consensus.enableCanonicalTxOrder=0 -listen=false -server=false -reindex \
        -debug=dsproof
    
    And for node2 you'd use either of these two;
    
    $ bitcoin-qt -regtest -datadir=node2 -printtoconsole=true -debug=dsproof \
        -listen=true -consensus.enableCanonicalTxOrder=0 -server=false
    
    $ hub-qt -datadir=node2 -regtest -reindex -listen=true -server=false
    
    Then when both are at block height 115, using the debugging console,
    insert one transaction in each node software.
      Note; in BU you need to call 'generate' once as long as issue #2059 is
      unresolved.
    
    Order is relevant, you want to repeat the process in reverse order to
    make sure each is tested to be the one generating and propagating or
    the one downloading and validating.
    
    Use `sendrawtransaction`
    
    01000000010b9d14b709aa59bd594edca17db2951c6660ebc8daa31ceae233a5550314f158000000006b483045022100b34a120e69bc933ae16c10db0f565cb2da1b80a9695a51707e8a80c9aa5c22bf02206c390cb328763ab9ab2d45f874d308af2837d6d8cfc618af76744b9eeb69c3934121022708a547a1d14ba6df79ec0f4216eeec65808cf0a32f09ad1cf730b44e8e14a6ffffffff01faa7be00000000001976a9148438266ad57aa9d9160e99a046e39027e4fb6b2a88ac00000000
    
    01000000010b9d14b709aa59bd594edca17db2951c6660ebc8daa31ceae233a5550314f158000000006b483045022100d9d22406611228d64e6b674de8b16e802f8f789f8338130506c7741cdae9116602202dc63a4f5f9e750eec9dfc1557469bda43d3491b358484e5c25992a381048a494121022708a547a1d14ba6df79ec0f4216eeec65808cf0a32f09ad1cf730b44e8e14a6ffffffff01ea80be00000000001976a914a449b2bf8b8092a810ee3b4ba102037bf4b96d2288ac00000000

---
+++ src/DoubleSpendProof.cpp
@@ -0,0 +1,293 @@
+// Copyright (C) 2019-2020 Tom Zander <tomz@freedommail.ch>
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "DoubleSpendProof.h"
+#include "hashwrapper.h"
+#include "main.h"
+#include "pubkey.h"
+#include "script/interpreter.h"
+#include "txmempool.h"
+
+namespace {
+    enum ScriptType {
+        P2PKH
+    };
+
+    void getP2PKHSignature(const CScript &script, std::vector<uint8_t> &vchRet)
+    {
+        auto scriptIter = script.begin();
+        opcodetype type;
+        script.GetOp(scriptIter, type, vchRet);
+    }
+
+    void hashTx(DoubleSpendProof::Spender &spender, const CTransaction &tx, int inputIndex)
+    {
+        assert(!spender.pushData.empty());
+        assert(!spender.pushData.front().empty());
+        auto hashType = spender.pushData.front().back();
+        if (!(hashType & SIGHASH_ANYONECANPAY)) {
+            CHashWriter ss(SER_GETHASH, 0);
+            for (size_t n = 0; n < tx.vin.size(); ++n) {
+                ss << tx.vin[n].prevout;
+            }
+            spender.hashPrevOutputs = ss.GetHash();
+        }
+        if (!(hashType & SIGHASH_ANYONECANPAY) && (hashType & 0x1f) != SIGHASH_SINGLE
+                && (hashType & 0x1f) != SIGHASH_NONE) {
+            CHashWriter ss(SER_GETHASH, 0);
+            for (size_t n = 0; n < tx.vin.size(); ++n) {
+                ss << tx.vin[n].nSequence;
+            }
+            spender.hashSequence = ss.GetHash();
+        }
+        if ((hashType & 0x1f) != SIGHASH_SINGLE && (hashType & 0x1f) != SIGHASH_NONE) {
+            CHashWriter ss(SER_GETHASH, 0);
+            for (size_t n = 0; n < tx.vout.size(); ++n) {
+                ss << tx.vout[n];
+            }
+            spender.hashOutputs = ss.GetHash();
+        } else if ((hashType & 0x1f) == SIGHASH_SINGLE && inputIndex < tx.vout.size()) {
+            CHashWriter ss(SER_GETHASH, 0);
+            ss << tx.vout[inputIndex];
+            spender.hashOutputs = ss.GetHash();
+        }
+    }
+
+    class DSPSignatureChecker : public BaseSignatureChecker {
+    public:
+        DSPSignatureChecker(const DoubleSpendProof *proof, const DoubleSpendProof::Spender &spender, int64_t amount)
+            : m_proof(proof),
+              m_spender(spender),
+              m_amount(amount)
+        {
+        }
+
+        bool CheckSig(const std::vector<uint8_t> &vchSigIn, const std::vector<uint8_t> &vchPubKey, const CScript &scriptCode) const override {
+            CPubKey pubkey(vchPubKey);
+            if (!pubkey.IsValid())
+                return false;
+
+            std::vector<uint8_t> vchSig(vchSigIn);
+            if (vchSig.empty())
+                return false;
+            vchSig.pop_back(); // drop the hashtype byte tacked on to the end of the signature
+
+            CHashWriter ss(SER_GETHASH, 0);
+            ss << m_spender.txVersion << m_spender.hashPrevOutputs << m_spender.hashSequence;
+            ss <<  COutPoint(m_proof->prevTxId(), m_proof->prevOutIndex());
+            ss << static_cast<const CScriptBase &>(scriptCode);
+            ss << m_amount << m_spender.outSequence << m_spender.hashOutputs;
+            ss << m_spender.lockTime << (int) m_spender.pushData.front().back();
+            const uint256 sighash = ss.GetHash();
+
+            if (vchSig.size() == 64)
+                return pubkey.VerifySchnorr(sighash, vchSig);
+            return pubkey.VerifyECDSA(sighash, vchSig);
+        }
+        bool CheckLockTime(const CScriptNum&) const override {
+            return true;
+        }
+        bool CheckSequence(const CScriptNum&) const override {
+            return true;
+        }
+
+        const DoubleSpendProof *m_proof;
+        const DoubleSpendProof::Spender &m_spender;
+        const int64_t m_amount;
+    };
+}
+
+// static
+DoubleSpendProof DoubleSpendProof::create(const CTransaction &t1, const CTransaction &t2)
+{
+    DoubleSpendProof answer;
+    Spender &s1 = answer.m_spender1;
+    Spender &s2 = answer.m_spender2;
+
+    size_t inputIndex1 = 0;
+    size_t inputIndex2 = 0;
+    for (; inputIndex1 < t1.vin.size(); ++inputIndex1) {
+        const CTxIn &in1 = t1.vin.at(inputIndex1);
+        for (;inputIndex2 < t2.vin.size(); ++inputIndex2) {
+            const CTxIn &in2 = t2.vin.at(inputIndex2);
+            if (in1.prevout == in2.prevout) {
+                answer.m_prevOutIndex = in1.prevout.n;
+                answer.m_prevTxId = in1.prevout.hash;
+
+                s1.outSequence = in1.nSequence;
+                s2.outSequence = in2.nSequence;
+
+                // TODO pass in the mempool and find the prev-tx we spend
+                // then we can determine what script type we are dealing with and
+                // be smarter about finding the signature.
+                // Assume p2pkh for now.
+
+                s1.pushData.resize(1);
+                getP2PKHSignature(in1.scriptSig, s1.pushData.front());
+                s2.pushData.resize(1);
+                getP2PKHSignature(in2.scriptSig, s2.pushData.front());
+
+                auto hashType = s1.pushData.front().back();
+                if (!(hashType & SIGHASH_FORKID))
+                    throw std::runtime_error("Tx1 Not a Bitcoin Cash transaction");
+                hashType = s2.pushData.front().back();
+                if (!(hashType & SIGHASH_FORKID))
+                    throw std::runtime_error("Tx2 Not a Bitcoin Cash transaction");
+
+                break;
+            }
+        }
+    }
+
+    if (answer.m_prevOutIndex == -1)
+        throw std::runtime_error("Transactions do not double spend each other");
+    if (s1.pushData.front().empty() || s2.pushData.front().empty())
+        throw std::runtime_error("Transactions not using known payment type. Could not find sig");
+
+    s1.txVersion = t1.nVersion;
+    s2.txVersion = t2.nVersion;
+    s1.lockTime = t1.nLockTime;
+    s2.lockTime = t2.nLockTime;
+
+    hashTx(s1, t1, inputIndex1);
+    hashTx(s2, t2, inputIndex2);
+
+    // sort the spenders to have proof be independent of the order of txs coming in
+    int diff = s1.hashOutputs.Compare(s2.hashOutputs);
+    if (diff == 0)
+        diff = s1.hashPrevOutputs.Compare(s2.hashPrevOutputs);
+    if (diff > 0)
+        std::swap(s1, s2);
+
+    return answer;
+}
+
+DoubleSpendProof::DoubleSpendProof()
+{
+}
+
+bool DoubleSpendProof::isEmpty() const
+{
+    return m_prevOutIndex == -1 || m_prevTxId.IsNull();
+}
+
+DoubleSpendProof::Validity DoubleSpendProof::validate() const
+{
+    if (m_prevTxId.IsNull() || m_prevOutIndex < 0)
+        return Invalid;
+    if (m_spender1.pushData.empty() || m_spender1.pushData.front().empty()
+            || m_spender2.pushData.empty() || m_spender2.pushData.front().empty())
+        return Invalid;
+
+    // check if ordering is proper
+    int diff = m_spender1.hashOutputs.Compare(m_spender2.hashOutputs);
+    if (diff == 0)
+        diff = m_spender1.hashPrevOutputs.Compare(m_spender2.hashPrevOutputs);
+    if (diff > 0)
+        return Invalid;
+
+    // Get the previous output we are spending.
+    int64_t amount;
+    CScript prevOutScript;
+    auto prevTx = mempool.get(m_prevTxId);
+    if (prevTx.get()) {
+        if (prevTx->vout.size() <= m_prevOutIndex)
+            return Invalid;
+        auto output = prevTx->vout.at(m_prevOutIndex);
+        amount = output.nValue;
+        prevOutScript = output.scriptPubKey;
+    } else {
+        Coin coin;
+        if (!pcoinsTip->GetCoin({m_prevTxId, m_prevOutIndex}, coin)) {
+            /* if the output we spend is missing then either the tx just got mined
+             * or, more likely, our mempool just doesn't have it.
+             */
+            return MissingUTXO;
+        }
+        amount = coin.out.nValue;
+        prevOutScript = coin.out.scriptPubKey;
+    }
+
+    /*
+     * Find the matching transaction spending this. Possibly identical to one
+     * of the sides of this DSP.
+     * We need this because we want the public key that it contains.
+     */
+    CTransaction tx;
+    {
+        READLOCK(mempool.cs_txmempool);
+        auto it = mempool.mapNextTx.find({m_prevTxId, m_prevOutIndex});
+        if (it == mempool.mapNextTx.end() || m_prevOutIndex >= it->second.ptx->vout.size()) {
+            return MissingTransaction;
+        }
+        tx = *(it->second.ptx);
+    }
+
+    /*
+     * TomZ: At this point (2019-07) we only support P2PKH payments.
+     *
+     * Since we have an actually spending tx, we could trivially support various other
+     * types of scripts because all we need to do is replace the signature from our 'tx'
+     * with the one that comes from the DSP.
+     */
+    ScriptType scriptType = P2PKH; // FUTURE: look at prevTx to find out script-type
+
+    std::vector<uint8_t> pubkey;
+    for (size_t i = 0; i < tx.vin.size(); ++i) {
+        if (tx.vin[i].prevout.n == m_prevOutIndex
+                && tx.vin[i].prevout.hash == m_prevTxId) {
+
+            // Found the input script we need!
+
+            CScript inScript = tx.vin[i].scriptSig;
+            auto scriptIter = inScript.begin();
+            opcodetype type;
+            inScript.GetOp(scriptIter, type); // P2PKH: first signature
+            inScript.GetOp(scriptIter, type, pubkey); // then pubkey
+            break;
+        }
+    }
+
+    if (pubkey.empty())
+        return Invalid;
+
+    CScript inScript;
+    if (scriptType == P2PKH) {
+        inScript << m_spender1.pushData.front();
+        inScript << pubkey;
+    }
+    DSPSignatureChecker checker1(this, m_spender1, amount);
+    ScriptError_t error;
+    if (!VerifyScript(inScript, prevOutScript, 0 /*flags*/, MAX_OPS_PER_SCRIPT, checker1, &error)) {
+        LOGA("DoubleSpendProof failed validating first tx due to %s\n", ScriptErrorString(error));
+        return Invalid;
+    }
+
+    inScript.clear();
+    if (scriptType == P2PKH) {
+        inScript << m_spender2.pushData.front();
+        inScript << pubkey;
+    }
+    DSPSignatureChecker checker2(this, m_spender2, amount);
+    if (!VerifyScript(inScript, prevOutScript, 0 /*flags*/, MAX_OPS_PER_SCRIPT, checker2, &error)) {
+        LOGA("DoubleSpendProof failed validating second tx due to %s\n", ScriptErrorString(error));
+        return Invalid;
+    }
+    return Valid;
+}
+
+uint256 DoubleSpendProof::prevTxId() const
+{
+    return m_prevTxId;
+}
+
+int DoubleSpendProof::prevOutIndex() const
+{
+    return m_prevOutIndex;
+}
+
+uint256 DoubleSpendProof::createHash() const
+{
+    return SerializeHash(*this);
+}
