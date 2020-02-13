21e027b591e7e6721ce468e88d7973c0f10a4d3b
Greg-Griffith <probablyaplebeian@protonmail.com>
2020-02-13 09:11:58 -0800

    remove unused state param from updatecoins and spendcoins

--- src/test/coins_tests.cpp
+++ src/test/coins_tests.cpp
@@ -17,11 +17,7 @@
 #include <boost/test/unit_test.hpp>
 
 int ApplyTxInUndo(Coin &&undo, CCoinsViewCache &view, const COutPoint &out);
-void UpdateCoins(const CTransaction &tx,
-    CValidationState &state,
-    CCoinsViewCache &inputs,
-    CTxUndo &txundo,
-    int nHeight);
+void UpdateCoins(const CTransaction &tx, CCoinsViewCache &inputs, CTxUndo &txundo, int nHeight);
 
 namespace
 {
