55c9080a3e4a5e95c05ed58c788989e99e4c86b0
Andrea Suisani <sickpig@gmail.com>
2020-03-03 12:35:06 +0100

    Reformat MANDATORY_SCRIPT_VERIFY_FLAGS and add clang-format exception

--- src/script/standard.h
+++ src/script/standard.h
@@ -41,9 +41,13 @@ extern unsigned nMaxDatacarrierBytes;
  * Failing one of these tests may trigger a DoS ban - see CheckInputs() for
  * details.
  */
-static const uint32_t MANDATORY_SCRIPT_VERIFY_FLAGS = SCRIPT_VERIFY_P2SH | SCRIPT_VERIFY_STRICTENC |
-                                                      SCRIPT_ENABLE_SIGHASH_FORKID | SCRIPT_VERIFY_LOW_S |
+/* clang-format off */
+static const uint32_t MANDATORY_SCRIPT_VERIFY_FLAGS = SCRIPT_VERIFY_P2SH |
+                                                      SCRIPT_VERIFY_STRICTENC |
+                                                      SCRIPT_ENABLE_SIGHASH_FORKID |
+                                                      SCRIPT_VERIFY_LOW_S |
                                                       SCRIPT_VERIFY_NULLFAIL;
+/* clang-format on */
 
 enum txnouttype
 {
