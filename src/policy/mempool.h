93cc850908047d2e5fe5c6f305b831d92b9dcc3b
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2020-03-06 18:13:20 +0100

    Add new post-fork ancestor and descendants limit.
    Adds a getter that returns the correct default limit based on phonon activation status.
    
    Test Plan: make check
    
    This is a port of https://reviews.bitcoinabc.org/D5243

--- src/policy/mempool.h
+++ src/policy/mempool.h
@@ -4,6 +4,13 @@
 #ifndef BITCOIN_POLICY_MEMPOOL_H
 #define BITCOIN_POLICY_MEMPOOL_H
 
+#include <cstdint>
+
+class CBlockIndex;
+namespace Consensus {
+struct Params;
+}
+
 /** BU's default for -limitancestorcount, max number of in-mempool ancestors */
 static const unsigned int BU_DEFAULT_ANCESTOR_LIMIT = 500;
 /** BU's default for -limitancestorsize, maximum kilobytes of tx + all in-mempool ancestors. */
@@ -22,5 +29,13 @@ static const unsigned int BCH_DEFAULT_ANCESTOR_SIZE_LIMIT = 101;
 static const unsigned int BCH_DEFAULT_DESCENDANT_LIMIT = 25;
 /** Default for the maximum kilobytes of in-mempool descendants */
 static const unsigned int BCH_DEFAULT_DESCENDANT_SIZE_LIMIT = 101;
+
+/** New limit for chain of unconfirmed transaction that would be used once May 2020 upgrade will be active*/
+static const unsigned int BCH_DEFAULT_ANCESTOR_LIMIT_LONGER = 50;
+static const unsigned int BCH_DEFAULT_DESCENDANT_LIMIT_LONGER = 50;
+
+uint32_t GetBCHDefaultAncestorLimit(const Consensus::Params &params, const CBlockIndex *pindexPrev);
+uint32_t GetBCHDefaultDescendantLimit(const Consensus::Params &params, const CBlockIndex *pindexPrev);
+
 
 #endif // BITCOIN_POLICY_MEMPOOL_H
