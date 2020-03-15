0eb33f062560c28bd82bc2629d73e25c0bcdc44e
Calin Culianu <calin.culianu@gmail.com>
2020-03-15 07:32:09 +0200

    This will come in handy in a short while for benchmarking new UniValue code changes/optimizations.
    I added a new benchmark alongside the old "RpcMempool" benchmark (which
    creates a tiny 1k mempool with simple tx's in it), called
    "RpcMempool10k" which adds a similar test but with a larger mempool of
    10k tx's, each tx being more comples (10 ins, 10 outs).
    
    This new benchmark is a better test of UniValue -> JSON, amongst other
    things.
    
    The new benchmark takes ~21 seconds on my system to execute.
    
    When the new optimizations that for UniValue are being prepared are in
    place, that number will drop down to the expected 5 (all the other tests
    take 5 seconds on my system to execute, give or take 1 second).
    
    Note we need this in place in master before my upcoming changes to
    UniValue to make testing easier -- you can switch between master and my
    patches to characterize the effects of MR's I plan on submitting.
    
    This commit maintains the predicate that we don't modify old benchmarks
    for the time being (so as to compare regressions to ABC), and that we
    only add new benchmarks alongside existing ones.

--- src/bench/rpc_mempool.cpp
+++ src/bench/rpc_mempool.cpp
@@ -47,4 +47,36 @@ static void RpcMempool(benchmark::State &state)
     }
 }
 
+static void RpcMempool10k(benchmark::State &state)
+{
+    CTxMemPool pool(CFeeRate(1000));
+
+    const size_t nTx = 10000, nIns = 10, nOuts = 10;
+
+    for (size_t i = 0; i < nTx; ++i)
+    {
+        CMutableTransaction tx = CMutableTransaction();
+        tx.vin.resize(nIns);
+        for (size_t j = 0; j < nIns; ++j)
+        {
+            tx.vin[j].scriptSig = CScript() << OP_1;
+        }
+        tx.vout.resize(nOuts);
+        for (size_t j = 0; j < nOuts; ++j)
+        {
+            tx.vin[j].scriptSig = CScript() << OP_1;
+            tx.vout[j].scriptPubKey = CScript() << OP_1 << OP_EQUAL;
+            tx.vout[j].nValue = int64_t(i * j) * CENT;
+        }
+        const CTransactionRef tx_r{MakeTransactionRef(tx)};
+        AddTx(tx_r, /* fee */ int64_t(i) * CENT, pool);
+    }
+
+    while (state.KeepRunning())
+    {
+        (void)mempoolToJSON(true);
+    }
+}
+
 BENCHMARK(RpcMempool, 40);
+BENCHMARK(RpcMempool10k, 10);
