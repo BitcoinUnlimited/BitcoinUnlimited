288f738ff6f9b9340d8371772bf5dd7efb1ecdd5
Andrea Suisani <sickpig@gmail.com>
2020-03-31 18:38:36 +0200

    [rebrand] Update CLIENT_NAME in clientversion.cpp

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -16,7 +16,7 @@
  * for both bitcoind and bitcoin-core, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("BUCash");
+const std::string CLIENT_NAME("BCH Unlimited");
 
 // BU added
 /**
