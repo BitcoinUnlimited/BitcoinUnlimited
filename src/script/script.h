4b4bd013d2a3f5e3ebfe6d6b898420713b83c410
Andrew Stone <g.andrew.stone@gmail.com>
2020-04-09 21:23:07 -0400

    fix enum misordering (#2162)

--- src/script/script.h
+++ src/script/script.h
@@ -186,12 +186,12 @@ enum opcodetype
     OP_NOP9 = 0xb8,
     OP_NOP10 = 0xb9,
 
-    // additional byte string operations
-    OP_REVERSEBYTES = 0xbc,
-
     // More crypto
     OP_CHECKDATASIG = 0xba,
     OP_CHECKDATASIGVERIFY = 0xbb,
+
+    // additional byte string operations
+    OP_REVERSEBYTES = 0xbc,
 
     // The first op_code value after all defined opcodes
     FIRST_UNDEFINED_OP_VALUE,
