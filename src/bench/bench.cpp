439a25c8604a199b3e1db6eabed6a3e2e129e522
Wladimir J. van der Laan <laanwj@gmail.com>
2020-04-30 11:58:49 +0200

    Log debug build status and warn when running benchmarks
    Log whether the starting instance of bitcoin core is a debug or release
    build (--enable-debug).
    
    Also warn when running the benchmarks with a debug build, to prevent
    mistakes comparing debug to non-debug results.
    
    This is a port of Core #12197 (1st commit)

--- src/bench/bench.cpp
+++ src/bench/bench.cpp
@@ -110,6 +110,10 @@ void benchmark::BenchRunner::RunAll(Printer &printer,
         std::cerr << "WARNING: Clock precision is worse than microsecond - benchmarks may be less accurate!\n";
     }
 
+#ifdef DEBUG
+    std::cerr << "WARNING: This is a debug build - may result in slower benchmarks.\n";
+#endif
+
     std::regex reFilter(filter);
     std::smatch baseMatch;
 
