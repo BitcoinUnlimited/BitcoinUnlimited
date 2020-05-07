1c6e49286c1fd174bff256831999f1741f984201
Greg-Griffith <probablyaplebeian@protonmail.com>
2020-05-07 16:22:01 -0700

    update testnet suite to match new spec

--- qa/pull-tester/rpc-tests.py
+++ qa/pull-tester/rpc-tests.py
@@ -258,6 +258,7 @@ testScripts = [ RpcTest(t) for t in [
     'graphene_stage2',
     'thinblocks',
     Disabled('checkdatasig_activation', "CDSV has been already succesfully activated, keep test around as a template for other OP activation"),
+    'xversion_old',
     'xversion',
     'sighashmatch',
     'getlogcategories',
