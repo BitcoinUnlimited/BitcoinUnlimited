4be3ae217fc4121e4f54c7eb5a10d1ae7c328377
Greg-Griffith <probablyaplebeian@protonmail.com>
2020-05-13 23:03:11 -0700

    implement basic subblock class

---
+++ src/bobtail/subblock.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2020 The Bitcoin Unlimited developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "subblock.h"
+
+void CSubBlock::SetNull()
+{
+    CBlockHeader::SetNull();
+    vtx.clear();
+}
+
+bool CSubBlock::IsNull() const
+{
+    return (vtx.empty() && CBlockHeader::IsNull());
+}
+
+CBlockHeader CSubBlock::GetBlockHeader() const
+{
+    CBlockHeader block;
+    block.nVersion = nVersion;
+    block.hashPrevBlock = hashPrevBlock;
+    block.hashMerkleRoot = hashMerkleRoot;
+    block.nTime = nTime;
+    block.nBits = nBits;
+    block.nNonce = nNonce;
+    return block;
+}
+
+std::string CSubBlock::ToString() const
+{
+    std::stringstream s;
+    s << strprintf(
+        "CSubBlock(hash=%s, ver=%d, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, vtx=%u)\n",
+        GetHash().ToString(), nVersion, hashPrevBlock.ToString(), hashMerkleRoot.ToString(), nTime, nBits, nNonce,
+        vtx.size());
+    for (unsigned int i = 0; i < vtx.size(); i++)
+    {
+        s << "  " << vtx[i]->ToString() << "\n";
+    }
+    return s.str();
+}
---
+++ src/bobtail/subblock.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2020 The Bitcoin Unlimited developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_BOBTAIL_SUBBLOCK_H
+#define BITCOIN_BOBTAIL_SUBBLOCK_H
+
+#include "primitives/block.h"
+
+class CSubBlock : public CBlockHeader
+{
+public:
+    std::vector<CTransactionRef> vtx;
+
+    CSubBlock() { SetNull(); }
+
+    CSubBlock(const CBlockHeader &header)
+    {
+        SetNull();
+        *((CBlockHeader *)this) = header;
+    }
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream &s, Operation ser_action)
+    {
+        READWRITE(*(CBlockHeader *)this);
+        READWRITE(vtx);
+    }
+
+    void SetNull();
+
+    bool IsNull() const;
+
+    CBlockHeader GetBlockHeader() const;
+
+    std::string ToString() const;
+};
+
+
+#endif
