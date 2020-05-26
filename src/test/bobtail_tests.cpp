b6e615545e4fa6454755442f42b5b65742d3c765
George Bissias <bissias@gmail.com>
2020-05-26 11:07:15 -0400

    add unit test file for bobtail

---
+++ src/test/bobtail_tests.cpp
@@ -0,0 +1,20 @@
+#include "bobtail/dag.h"
+#include "bobtail/subblock.h"
+#include "test/test_bitcoin.h"
+#include <boost/test/unit_test.hpp>
+
+BOOST_FIXTURE_TEST_SUITE(bobtail_tests, BasicTestingSetup)
+
+BOOST_AUTO_TEST_CASE(test_dag_temporal_sort)
+{
+    CDagForrest forest;
+    CSubBlock subblock1;
+    CSubBlock subblock2;
+    forest.Insert(subblock1);
+    forest.Insert(subblock2);
+    forest.TemporalSort();
+
+    BOOST_CHECK(forest.IsTemporallySorted());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
