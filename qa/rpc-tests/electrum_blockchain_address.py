d723fb4335d1ca722735d550bee7b769a3563da2
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2020-06-24 10:52:20 +0000

    [qa] Add tests for blockchain.*.get_first_use

--- qa/rpc-tests/electrum_blockchain_address.py
+++ qa/rpc-tests/electrum_blockchain_address.py
@@ -11,6 +11,7 @@ from test_framework.loginit import logging
 from test_framework.electrumutil import compare, bitcoind_electrum_args, \
     address_to_scripthash, sync_electrum_height, ElectrumConnection
 from test_framework.nodemessages import COIN, CTransaction, ToHex, CTxIn, COutPoint
+from test_framework.connectrum.exc import ElectrumErrorResponse
 
 
 class ElectrumBlockchainAddress(BitcoinTestFramework):
@@ -32,11 +33,11 @@ class ElectrumBlockchainAddress(BitcoinTestFramework):
         n.generate(200)
 
         async def async_tests():
+            await self.test_get_frist_use(n)
             cli = ElectrumConnection()
             await cli.connect()
             await self.test_invalid_args(cli)
             await self.test_get_balance(n, cli)
-            await self.test_get_frist_use(n, cli)
             await self.test_get_history(n, cli)
             await self.test_list_unspent(n, cli)
         loop = asyncio.get_event_loop()
@@ -75,19 +76,77 @@ class ElectrumBlockchainAddress(BitcoinTestFramework):
         n.generate(1)
         await waitForAsync(10, lambda: check_address(addr, confirmed = balance))
 
-    async def test_get_frist_use(self, n, cli):
+    async def sendtoaddr(self, n, cli, addr, amount):
+        utxo = n.listunspent().pop()
+        inputs = [{
+            "txid": utxo["txid"],
+            "vout": utxo["vout"]}]
+        outputs = {
+            addr: utxo['amount'],
+        }
+        tx = n.createrawtransaction(inputs, outputs)
+        signed = n.signrawtransaction(tx)
+        txid = await cli.call("blockchain.transaction.broadcast", signed['hex'])
+        return txid
+
+    async def test_get_frist_use(self, n):
+        cli = ElectrumConnection()
+        await cli.connect()
+
+        # New address that has never received coins. Should return an error.
         addr = n.getnewaddress()
-        n.sendtoaddress(addr, 12)
+        await assert_raises_async(
+            ElectrumErrorResponse,
+            cli.call,
+            "blockchain.address.get_first_use", addr)
+        await assert_raises_async(
+            ElectrumErrorResponse,
+            cli.call,
+            "blockchain.scripthash.get_first_use", address_to_scripthash(addr))
 
-        utxo = [ ]
-        async def fetch_utxo():
+        # Send coin to the new address
+        txid = await self.sendtoaddr(n, cli, addr, 1)
+
+        # Wait for electrum server to see the utxo.
+        async def wait_for_utxo():
             utxo = await cli.call("blockchain.address.listunspent", addr)
-            return len(utxo) > 0
+            if len(utxo) == 1:
+                return utxo
+            return None
+        utxo = await waitForAsync(10, wait_for_utxo)
 
-        await waitForAsync(10, fetch_utxo)
-        #firstuse = await cli.call("blockchain.address.get_first_use", addr)
+        # Observe that get_first_use returns the tx when it's in the mempool
+        res = await cli.call("blockchain.address.get_first_use", addr)
+        res2 = await cli.call("blockchain.scripthash.get_first_use",
+                address_to_scripthash(addr))
+        assert_equal(res, res2)
+        assert_equal(
+            "0000000000000000000000000000000000000000000000000000000000000000",
+            res['block_hash'])
+        assert_equal(0, res['height'])
+        assert_equal(txid, res['tx_hash'])
 
-        # NYI. Api not stable.
+        # Confirm tx, observe that block height and gets set.
+        n.generate(1)
+        sync_electrum_height(n)
+        res = await cli.call("blockchain.address.get_first_use", addr)
+        res2 = await cli.call("blockchain.scripthash.get_first_use",
+                address_to_scripthash(addr))
+        assert_equal(res, res2)
+        assert_equal(n.getbestblockhash(), res['block_hash'])
+        assert_equal(n.getblockcount(), res['height'])
+        assert_equal(txid, res['tx_hash'])
+
+        # Send another tx, observe that the first one is till returned.
+        txid2 = await self.sendtoaddr(n, cli, addr, 2)
+        res = await cli.call("blockchain.address.get_first_use", addr)
+        assert_equal(txid, res['tx_hash'])
+
+        # Also when the second tx is confirmed, the first is returned.
+        n.generate(1)
+        sync_electrum_height(n)
+        res = await cli.call("blockchain.address.get_first_use", addr)
+        assert_equal(txid, res['tx_hash'])
 
     async def test_list_unspent(self, n, cli):
         addr = n.getnewaddress()
