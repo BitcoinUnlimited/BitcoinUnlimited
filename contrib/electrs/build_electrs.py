0f1e849adca759e22d6a4e202c9830678583fda5
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2020-08-25 09:25:38 +0200

    Bump ElectrsCash HEAD commit hash
    A new commit was pushed to the v2.0 branch of ElectrsCash to fix an
    crates dependency issue.

--- contrib/electrs/build_electrs.py
+++ contrib/electrs/build_electrs.py
@@ -11,7 +11,7 @@ GIT_REPO = "https://github.com/BitcoinUnlimited/{}.git".format(PROJECT_NAME)
 GIT_BRANCH = "v2.0.0"
 # When released put a hash here: "aa95d64d050c286356dadb78d19c2e687dec85cf"
 # When in development, put 'None' here
-EXPECT_HEAD = "7dc599c09bf2f7604e2bdd7ecf613381c77e3092"
+EXPECT_HEAD = "6cf6a66b5bc5ce38d3116812d4f32457984a975e"
 
 ROOT_DIR = os.path.realpath(
         os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
