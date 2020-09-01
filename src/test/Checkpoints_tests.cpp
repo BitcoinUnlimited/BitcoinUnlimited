9f46b150b42972142851f2ff605e76e6b64ece6a
ptschip <peter.tschipper@gmail.com>
2020-09-01 03:00:39 -0700

    Re-enable GetLastCheckpoint()

--- src/test/Checkpoints_tests.cpp
+++ src/test/Checkpoints_tests.cpp
@@ -17,12 +17,32 @@
 
 using namespace std;
 
-BOOST_FIXTURE_TEST_SUITE(Checkpoints_tests, BasicTestingSetup)
+BOOST_FIXTURE_TEST_SUITE(Checkpoints_tests, TestChain100Setup)
 
 BOOST_AUTO_TEST_CASE(sanity)
 {
+    // Test Get Total Block Estimate
     const CCheckpointData &checkpoints = Params(CBaseChainParams::MAIN).Checkpoints();
     BOOST_CHECK(Checkpoints::GetTotalBlocksEstimate(checkpoints) >= 134444);
+
+
+    // Test the finding of the last checkpoint
+    uint256 hash1 = chainActive.Tip()->pprev->pprev->GetBlockHash();
+    uint256 hash2 = chainActive.Tip()->pprev->GetBlockHash();
+    uint256 hash_last_checkpoint = chainActive.Tip()->GetBlockHash();
+
+    CCheckpointData checkpoints2;
+    checkpoints2.mapCheckpoints[1] = hash1;
+    checkpoints2.mapCheckpoints[2] = hash2;
+    checkpoints2.mapCheckpoints[3] = hash_last_checkpoint;
+
+    READLOCK(cs_mapBlockIndex);
+    CBlockIndex *pindex2 = Checkpoints::GetLastCheckpoint(checkpoints2);
+    BOOST_CHECK(pindex2 != nullptr);
+    if (pindex2)
+    {
+        BOOST_CHECK(pindex2->GetBlockHash() == hash_last_checkpoint);
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
