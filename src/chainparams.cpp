496ac2174ba41438add68e70779621928c79e04b
Justaphf <juholmes@vt.edu>
2020-09-20 13:56:11 -0400

    [PORT] Add testnet4 IPv6 Seeds from BCHN

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -622,7 +622,7 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
         cashaddrPrefix = "bchtest";
-        vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_test, pnSeed6_test + ARRAYLEN(pnSeed6_test));
+        vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_test4, pnSeed6_test4 + ARRAYLEN(pnSeed6_test4));
 
         fMiningRequiresPeers = true;
         fDefaultConsistencyChecks = false;
