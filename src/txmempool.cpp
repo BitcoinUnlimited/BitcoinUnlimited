15e8a1f2f5d7000529eba5e6b257088573abe9eb
Peter Tschipper <peter.tschipper@gmail.com>
2020-12-16 05:38:43 -0400

    Change the place where we check for nTxPersec < 0
    We need to do this check just after decrememnting nTxPerSec and
    not where wher increment it.  Otherwise we can end up not setting
    it's value to zero when we need to.

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -1885,6 +1885,8 @@ void CTxMemPool::UpdateTransactionsPerSecondImpl(bool fAddTxn, const std::lock_g
     {
         nTxPerSec -= (nTxPerSec / TX_RATE_SMOOTHING_SEC) * nDeltaTime;
         nLastTime = nNow;
+        if (nTxPerSec < 0)
+            nTxPerSec = 0;
     }
 
     // Add the new tx to the rate
@@ -1893,8 +1895,6 @@ void CTxMemPool::UpdateTransactionsPerSecondImpl(bool fAddTxn, const std::lock_g
         nCount++;
         // The amount that the new tx will add to the tx rate
         nTxPerSec += 1 / TX_RATE_SMOOTHING_SEC;
-        if (nTxPerSec < 0)
-            nTxPerSec = 0;
     }
 
     // Calculate the peak rate if we've gone more that 1 second beyond the last sample time.
