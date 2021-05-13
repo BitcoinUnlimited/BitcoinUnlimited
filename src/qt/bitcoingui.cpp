eaf46c647f30ceb96989bad1a62297f5318b88cf
Griffith <probablyaplebeian@protonmail.com>
2021-05-13 06:46:03 -0700

    Only consider "Up to date" when receiving a recent block, not a header.

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -750,7 +750,7 @@ void BitcoinGUI::setNumBlocks(int count, const QDateTime &blockDate, double nVer
     tooltip = tr("Processed %n block(s) of transaction history.", "", count);
 
     // Set icon state: spinning if catching up, tick otherwise
-    if (secs < 90 * 60)
+    if (fHeader == false && secs < 90 * 60)
     {
         tooltip = tr("Up to date") + QString(".<br>") + tooltip;
         labelBlocksIcon->setPixmap(
