2bc9250b93e9690616b7c17c0433d13f1038f606
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2022-09-29 10:06:39 +0200

    build: Bump rust to 1.64.0

--- depends/packages/rust.mk
+++ depends/packages/rust.mk
@@ -1,8 +1,8 @@
 package=rust
-$(package)_version=1.59.0
+$(package)_version=1.64.0
 $(package)_download_path=https://static.rust-lang.org/dist
 $(package)_file_name=rust-$($(package)_version)-x86_64-unknown-linux-gnu.tar.gz
-$(package)_sha256_hash=0c1c2da3fa26372e5178123aa5bb0fdcd4933fbad9bfb268ffbd71807182ecae
+$(package)_sha256_hash=a893977f238291370ab96726a74b6b9ae854dc75fbf5730954d901a93843bf9b
 
 define $(package)_stage_cmds
   ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native --disable-ldconfig
