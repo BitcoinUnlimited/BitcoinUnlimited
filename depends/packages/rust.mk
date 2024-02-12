5d2c1d1e832d456db1b0260f3b353c40ac314875
Andrea Suisani <sickpig@gmail.com>
2024-02-12 18:14:55 +0100

    [depends] update rust to last stable version, 1.73.0
    This is needed to fix rostrum compilation, see:
    
    https://gitlab.com/bitcoinunlimited/BCHUnlimited/-/jobs/6153402354
    
    for more details.

--- depends/packages/rust.mk
+++ depends/packages/rust.mk
@@ -1,8 +1,8 @@
 package=rust
-$(package)_version=1.64.0
+$(package)_version=1.73.0
 $(package)_download_path=https://static.rust-lang.org/dist
 $(package)_file_name=rust-$($(package)_version)-x86_64-unknown-linux-gnu.tar.gz
-$(package)_sha256_hash=a893977f238291370ab96726a74b6b9ae854dc75fbf5730954d901a93843bf9b
+$(package)_sha256_hash=aa4cf0b7e66a9f5b7c623d4b340bb1ac2864a5f2c2b981f39f796245dc84f2cb
 
 define $(package)_stage_cmds
   ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native --disable-ldconfig
