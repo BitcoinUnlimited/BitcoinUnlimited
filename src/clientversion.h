1bd0ff43df7be2dc822e1137bcccb12d7098d9f7
Andrea Suisani <sickpig@gmail.com>
2024-02-23 14:30:14 +0100

    Bump BCHU version to 2.0.0.1

--- src/clientversion.h
+++ src/clientversion.h
@@ -18,7 +18,7 @@
 #define CLIENT_VERSION_MAJOR 2
 #define CLIENT_VERSION_MINOR 0
 #define CLIENT_VERSION_REVISION 0
-#define CLIENT_VERSION_BUILD 0
+#define CLIENT_VERSION_BUILD 1
 
 //! Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE true
@@ -27,7 +27,7 @@
  * Copyright year (2009-this)
  * Todo: update this when changing our copyright comments in the source
  */
-#define COPYRIGHT_YEAR 2023
+#define COPYRIGHT_YEAR 2024
 
 #endif // HAVE_CONFIG_H
 
